bb9faadb7dac2fd3d4ec3705a1cd562a8c892cfa
ssungho <thtl98@naver.com>
2023-03-07 20:17:06 +0900

    병합 알고리즘, 검색 알고리즘
    날짜 : 2023/03/07
    
    병합(Merge) 알고리즘,
    검색(Search) 알고리즘
    
    MergeAlgorithm.cs 추가,
    SearchAlgorithm.cs 추가,

---
+++ CSharp_Algorithms/Algorithms/MergeAlgorithm/MergeAlgorithm.cs
@@ -0,0 +1,45 @@
+﻿// [?] 2개의 정수 배열 합치기
+using System;
+// 병합 알고리즘: 정렬된 두 개의 배열을 하나로 병합
+class MergeAlgorithm
+{
+    static void Main()
+    {
+        // [1] Inupt
+        int[] first = { 1, 3, 5 }; // 오름차순 정렬
+        int[] second = { 2, 4 }; // 오름차순 정렬
+        int M = first.Length;
+        int N = second.Length;
+        int[] merge = new int[M + N]; // 병합된 배열을 담을 그릇
+        int i = 0; int j = 0; int k = 0;
+
+        // [2] Process
+        while (i < M && j < N)  // 둘중 하나라도 배열의 끝에 도착할 때 까지
+        {
+            if(first[i] <= second[j]) // 더 작은 값을 merge 배열에 저장
+            {
+                merge[k++] = first[i++];
+            }
+            else
+            {
+                merge[k++] = second[j++];
+            }
+        }
+        while(i < M) // 첫 번째 배열이 끝까지 도달할 때 까지
+        {
+            merge[k++] = first[i++];
+        }
+        while(j< N) // 두 번째 배열이 끝까지 도달할 때 까지
+        {
+            merge[k++] = second[j++];
+        }
+
+        // [3] Output
+        foreach( var m in merge)
+        {
+            Console.Write($"{m}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ CSharp_Algorithms/Algorithms/SearchAlgorithm/SearchAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿// [?] 정렬되어 있는 데이터를 이진 검색을 사용하여 반씩 나눠서 검색
+using System;
+// 검색 알고리즘(SearchAlgorithm): 주어진 데이터에서 특정 데이터 찾기
+class SearchAlgorithm
+{
+    static void Main()
+    {
+        Console.Write("검색할 숫자를 입력하세요: \b");
+
+        // [1] Input
+        int[] data = { 1, 3, 5, 7, 9 }; // 오름차순으로 정렬되었다고 가정
+        int N = data.Length; // 의사코드
+        int search = Convert.ToInt32(Console.ReadLine()); // 검색할 데이터
+        bool flag = false; // 찾으면  true 찾지 못하면 false
+        int index = -1; // 인덱스 변수: 찾은 위치
+
+        // [2] Process: 이진 검색(Binary Search)
+        int low = 0; // 낮은 인덱스
+        int high = N - 1; // 높은 인덱스
+        while(low <= high)
+        {
+            int mid = (low + high) / 2; // 중간 인덱스 구하기
+            if (data[mid] == search)
+            {
+                flag = true;
+                index = mid;
+                break;
+            }
+             if (data[mid] > search)
+            {
+                high = mid - 1; // 찾을 데이터가 작으면 왼쪽 영역으로 이동
+            }
+            else
+            {
+                low = mid + 1; // 찾을 데이터가 크면 오른쪽 영역으로 이동
+            }
+        }
+
+        // [3] Output
+        if (flag)
+        {
+            Console.WriteLine($"{search}을(를) {index}위치에서 찾았습니다.");
+        }
+        else
+            Console.WriteLine("찾지 못했습니다.");
+    }
+}
+
