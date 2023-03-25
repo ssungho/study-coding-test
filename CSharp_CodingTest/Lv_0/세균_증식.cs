875b3567ec8123ec18c3a9ee5ed01177855026d1
ssungho <thtl98@naver.com>
2023-03-25 23:57:06 +0900

    2023/03/25
    lv0 문제 1개, lv1 문제 1개 해결.

---
+++ CSharp_CodingTest/Lv_0/세균_증식.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/// <summary>
+/// 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 
+/// 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+/// </summary>
+internal class 세균_증식
+{
+    public int solution(int n, int t)
+    {
+        int answer = 0;
+        int time = 1;
+        for (int i = 1; i <= t; i++)
+        {
+            time *= 2;
+        }
+        answer = n * time;
+
+        return answer;
+    }
+}
+
--- CSharp_CodingTest/Test.cs
+++ CSharp_CodingTest/Test.cs
@@ -2,16 +2,16 @@
 {
     static void Main(string[] args)
     {
-        문자열_계산하기 test = new();
+        세균_증식 test = new();
 
         while (true)
         {
             // int[] arr = { 3, 10, 28 };
-            string a = Console.ReadLine();
+            // string a = Console.ReadLine();
             // string b = Console.ReadLine();
-            // int a = Convert.ToInt32(Console.ReadLine());
-            // int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(test.solution(a));
+             int a = Convert.ToInt32(Console.ReadLine());
+             int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(test.solution(a, b));
             // int[] answer = test.solution(a);
             //foreach ( var c in answer)
             //{
