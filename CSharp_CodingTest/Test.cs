614f6d35e9a29cec9883695addb023ff02d56074
ssungho <thtl98@naver.com>
2023-04-05 00:27:58 +0900

    2023/04/04
    lv 0 저주의 문자 3

--- CSharp_CodingTest/Test.cs
+++ CSharp_CodingTest/Test.cs
@@ -2,17 +2,17 @@
 {
     static void Main(string[] args)
     {
-        이진수_더하기 test = new();
+        저주의_숫자_3 test = new();
 
         while (true)
         {
             // string[] ans = new string[2] { "3 - 4 = -3", "5 + 6 = 11" };
             // int[,] arr = { { -1, -1 }, { 1, 1 }, { 1, -1 },{ -1, 1 } };
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            // int a = Convert.ToInt32(Console.ReadLine());
+            // string a = Console.ReadLine();
+            // string b = Console.ReadLine();
+            int a = Convert.ToInt32(Console.ReadLine());
             // int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(test.solution(a, b));
+            Console.WriteLine(test.solution(a));
             // int[] answer = test.solution(a);
             //foreach ( var c in answer)
             //{
