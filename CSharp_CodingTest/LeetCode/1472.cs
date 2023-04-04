9aedf7f9638c925da2ca64df02d57c889d832c9d
ssungho <thtl98@naver.com>
2023-04-04 11:12:44 +0900

    mkdir LeetCode
    1472. Design Browser History,
    739. Daily Temperatures

---
+++ CSharp_CodingTest/LeetCode/1472.cs
@@ -0,0 +1,63 @@
+﻿public class Design_Browser_History
+{
+    public class BrowserHistory
+    {
+        public Node head;
+        public Node current;
+        public Node tail;
+
+        public BrowserHistory(string homepage, Node head = null, Node tail = null)
+        {
+            Node current = new Node(homepage);
+            this.head = current;
+            this.current = current;
+            this.tail = current;
+        }
+
+        public void Visit(string url)
+        {
+            Node newNode = new Node(url);
+            newNode.prev = current;
+            current.next = newNode;
+            current = newNode;
+            tail = newNode;
+        }
+
+        public string Back(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                current = current.prev;
+                if (current == null)
+                    current = head;
+            }
+            return current.val;
+        }
+
+        public string Forward(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                current = current.next;
+                if (current == null)
+                    current = tail;
+            }
+            return current.val;
+        }
+    }
+
+    public class Node
+    {
+        public string val;
+        public Node next;
+        public Node prev;
+
+        public Node(string val, Node next = null, Node prev = null)
+        {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
+
---
+++ CSharp_CodingTest/LeetCode/739.cs
@@ -0,0 +1,28 @@
+﻿public class Daily_Temperatures
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        // 정답으로 반환할 배열을 초기화한다.
+        int[] answer = new int[temperatures.Length];
+        // int로 구성된 튜플 스택을 생성한다.
+        Stack<(int, int)> stack = new Stack<(int, int)>();
+        // 주어진 배열만큼 반복 O(N)
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            // 스택이 0이 아니고, 스택의 Top(Peek())의 온도값이 현재 온도보다 작다면, 
+            while (stack.Count != 0 && stack.Peek().Item1 < temperatures[i])
+            {
+                // 저장 당시 i값을 prev_day에 임시 저장
+                int prev_day = stack.Peek().Item2;
+                // 스택에서 제거후
+                stack.Pop();
+                // 정답배열의 [prev_day]값은 현재(i) - 저장 당시(i값)
+                answer[prev_day] = i - prev_day;
+            }
+            // 스택에 현재 온도와 현재 날(?)을 저장한다.    
+            stack.Push((temperatures[i], i));
+        }
+        return answer;
+    }
+}
+
