918eda92c8c72e752e95c0908f8f6d1eb76cb396
ssungho <thtl98@naver.com>
2023-04-07 12:03:46 +0900

    2023/04/07
    TwoSum
    ver2

---
+++ CSharp_CodingTest/LeetCode/TwoSum.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    // 동일 인덱스를 안쓰게 하려면 어떻게 해야 할까? 
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i]) && dict[target - nums[i]] != i)
+                return new int[] { dict[target - nums[i]], i };
+            dict[nums[i]] = i;
+        }
+        return null;
+    }
+}
