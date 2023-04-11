576dec0216aaf0ab79217027ec10927037f21d31
ssungho <thtl98@naver.com>
2023-04-11 23:46:58 +0900

    2023/04/11
    lv0 치킨 쿠폰

---
+++ CSharp_CodingTest/Lv_0/치킨_쿠폰.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class 치킨_쿠폰
+{
+    public int solution(int chicken)
+    {
+        int answer = 0; // 정답
+        int coupon = chicken; // 쿠폰 ( 10개가 모이면 치킨 하나 + 쿠폰)
+
+        while (coupon >= 10)
+        {
+            if(coupon >= 10)
+            {
+                answer += coupon / 10;
+                chicken = coupon / 10;
+                coupon = chicken + coupon % 10;
+            }
+        }
+        return answer;
+    }
+}
+
