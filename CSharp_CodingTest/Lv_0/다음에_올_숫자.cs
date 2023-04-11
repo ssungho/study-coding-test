706bdfaf8e590804d73e2de04938c570efc1c675
ssungho <thtl98@naver.com>
2023-04-11 23:57:59 +0900

    2023/04/11
    lv0 다음에 올 숫자

---
+++ CSharp_CodingTest/Lv_0/다음에_올_숫자.cs
@@ -0,0 +1,19 @@
+﻿internal class 다음에_올_숫자
+{
+    public int solution(int[] common)
+    {
+        int answer = 0;
+        int a;
+        if(common[2] - common[1] == common[1] - common[0])
+        {
+            a = common[1] - common[0];
+            return common[common.Length -1] + a;
+        }
+        else
+        {
+            a = common[1] / common[0];
+            return common[common.Length -1] * a;
+        }
+    }
+}
+
