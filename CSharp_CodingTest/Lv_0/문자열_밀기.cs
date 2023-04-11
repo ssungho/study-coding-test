44e41f128fc7d1885042761fab5946af76b598e1
ssungho <thtl98@naver.com>
2023-04-11 23:46:30 +0900

    2023/04/11
    lv0 문자열 밀기

---
+++ CSharp_CodingTest/Lv_0/문자열_밀기.cs
@@ -0,0 +1,23 @@
+﻿internal class 문자열_밀기
+{
+    public int solution(string A, string B)
+    {
+        int cnt = 0;
+        while (cnt < A.Length)
+        {
+            string temp = "";
+            for (int i = A.Length - cnt; i < A.Length; i++)
+                temp += A[i];
+
+            for (int i = 0; i < A.Length - cnt; i++)
+                temp += A[i];
+
+            if (temp == B)
+                return cnt;
+            else
+                cnt++;
+        }
+        return -1;
+    }
+}
+
--- CSharp_CodingTest/Test.cs
+++ CSharp_CodingTest/Test.cs
@@ -2,16 +2,16 @@
 {
     static void Main(string[] args)
     {
-        유한소수_판별하기 test = new();
+        문자열_밀기 test = new();
 
         while (true)
         {
             // string[] ans = new string[2] { "3 - 4 = -3", "5 + 6 = 11" };
             // int[,] arr = { { -1, -1 }, { 1, 1 }, { 1, -1 },{ -1, 1 } };
-            // string a = Console.ReadLine();
-            // string b = Console.ReadLine();
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(test.solution(a, b));
             // int[] answer = test.solution(a);
             //foreach ( var c in answer)
