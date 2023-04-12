26147ad91691fd311e6bb5606610bd4a79753819
ssungho <thtl98@naver.com>
2023-04-12 23:35:27 +0900

    2023/04/12
    lv0 연속된 수의 합

---
+++ CSharp_CodingTest/Lv_0/연속된_수의_합.cs
@@ -0,0 +1,30 @@
+﻿internal class 연속된_수의_합
+{
+    public int[] solution(int num, int total)
+    {
+        int[] answer = new int[num];  
+        int center = total / num;
+        int cnt = 1;
+        for (int i = 0; i < num; i++)
+        { 
+            if (i < num / 2)
+                answer[num / 2 -1 - i] = center - i -1;
+            if (i == num / 2)
+                answer[num / 2] = center;
+            if (i > num / 2)
+            {
+                answer[i] = center + cnt;
+                cnt++;
+            }
+        }
+        int temp = 0;
+        for (int i = 0; i < num; i++)
+            temp += answer[i];
+
+        if (temp != total)
+            for (int i = 0; i < num; i++)
+                answer[i] += 1;
+        return answer;
+    }
+}
+
