d563a1ad5b2049b5fa8a18d79caa029fe8844039
ssungho <thtl98@naver.com>
2023-04-13 02:40:56 +0900

    2023/04/13
    lv0 옹알이

---
+++ CSharp_CodingTest/Lv_0/옹알이.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+internal class 옹알이
+{
+    public int solution(string[] babbling)
+    {
+        int answer = 0;
+        string[] can = new string[4] { "aya", "ye", "woo", "ma" };
+
+        for (int i = 0; i < babbling.Length; i++)
+        {
+            int cnt = 0;
+            while(cnt < 4)
+            {
+                if (babbling[i].Contains(can[cnt]))
+                    babbling[i] = babbling[i].Replace(can[cnt], " ");
+                cnt++;
+            }
+            if (babbling[i].Replace(" ","") == "")
+                answer++;
+        }
+        return answer;
+    }
+}
+
--- CSharp_CodingTest/Test.cs
+++ CSharp_CodingTest/Test.cs
@@ -2,17 +2,17 @@
 {
     static void Main(string[] args)
     {
-        문자열_밀기 test = new();
+        옹알이 test = new();
 
         while (true)
         {
-            // string[] ans = new string[2] { "3 - 4 = -3", "5 + 6 = 11" };
+            string[] ans = new string[5] { "aya", "yee", "u", "maa", "wyeoo" };
             // int[,] arr = { { -1, -1 }, { 1, 1 }, { 1, -1 },{ -1, 1 } };
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
+            // string a = Console.ReadLine();
+            // string b = Console.ReadLine();
             // int a = Convert.ToInt32(Console.ReadLine());
             // int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(test.solution(a, b));
+            Console.WriteLine(test.solution(ans));
             // int[] answer = test.solution(a);
             //foreach ( var c in answer)
             //{
