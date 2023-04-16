f44c73432d31b5abec6e4b29f1fce53cbe5de8e7
ssungho <thtl98@naver.com>
2023-04-16 23:12:54 +0900

    2023/04/16
    lv2 최솟값 최댓값

---
+++ CSharp_CodingTest/lv_2/최솟값과_최댓값.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace 최댓값과_최솟값
+{
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            string temp = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || i == s.Length - 1)
+                {
+                    if (i == s.Length - 1)
+                        temp += s[i];
+
+                    if (int.Parse(temp) >= max)
+                        max = int.Parse(temp);
+
+                    if (int.Parse(temp) <= min)
+                        min = int.Parse(temp);
+
+                    temp = "";
+                }
+                else
+                    temp += s[i];
+            }
+            return new string(min + " " + max);
+        }
+    }
+}
