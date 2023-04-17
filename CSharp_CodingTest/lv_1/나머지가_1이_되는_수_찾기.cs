b918229c56bf297a2ae4f4f039947f1bd017486e
ssungho <thtl98@naver.com>
2023-04-17 16:59:50 +0900

    2023/04/17
    lv1 나머지가 1이 되는 수 찾기

---
+++ CSharp_CodingTest/lv_1/나머지가_1이_되는_수_찾기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 나머지가_1이_되는_수_찾기
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int cnt = 1;
+            while (cnt < n)
+            {
+                if (n % cnt == 1)
+                    return cnt;
+                cnt++;
+            }
+            return cnt;
+        }
+    }
+}
