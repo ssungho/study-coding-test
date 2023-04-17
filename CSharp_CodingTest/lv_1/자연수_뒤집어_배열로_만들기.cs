1942075cbfe7549a0b9fd0ab7b121edf5c97e765
ssungho <thtl98@naver.com>
2023-04-17 21:55:37 +0900

    2023/04/17
    lv1 자연수 뒤집어 배열로 만들기

---
+++ CSharp_CodingTest/lv_1/자연수_뒤집어_배열로_만들기.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 자연수_뒤집어_배열로_만들기
+{
+    public class Solution
+    {
+        public int[] solution(long n)
+        {
+            List<int> nums = new List<int>();
+            
+            while(n >= 1)
+            {
+                long temp = n % 10;
+                nums.Add((int) temp);
+                n /= 10;
+            }
+
+            return nums.ToArray();
+        }
+    }
+}
