9fc5bfc4165d426a3a3dae226503de38739773f8
ssungho <thtl98@naver.com>
2023-04-19 23:51:33 +0900

    2023/04/19
    lv1 정수 제곱근 판별

---
+++ CSharp_CodingTest/lv_1/정수_제곱근_판별.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 정수_제곱근_판별
+{
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            // 양의 정수를 나타낼 변수
+            long num = 1;
+            // num이 n의 제곱근보다 커질때까지 반복
+            while(num <= Math.Sqrt(n))
+            {
+                // 반환 조건
+                if (num == Math.Sqrt(n))
+                    return (num + 1) * (num + 1);
+                // 카운트
+                else
+                    num++;
+            }
+            // 답이 없다면
+            return -1;
+        }
+    }
+}
