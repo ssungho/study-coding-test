52c3a5d8d8a424fdbc1fdcc39a731979f011db03
ssungho <thtl98@naver.com>
2023-04-20 22:23:42 +0900

    2023/04/20
    lv1 문자열을 정수로 바꾸기

---
+++ CSharp_CodingTest/lv_1/문자열을_정수로_바꾸기.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 문자열을_정수로_바꾸기
+{
+    public class Solution
+    {
+        public int solution(string s)
+        {
+            return int.Parse(s);
+        }
+    }
+}
