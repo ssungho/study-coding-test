1b2fe01fc3bd275e7467acfc15dfa3a8cd4d5616
ssungho <thtl98@naver.com>
2023-04-21 19:40:31 +0900

    2023/04/21
    lv1 정수 내림차순으로 정렬하기,
    lv1 하샤드 수

---
+++ CSharp_CodingTest/Programmers/lv_1/정수_내림차순으로_정렬하기.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_CodingTest.Programmers.lv_1
+{
+    internal class 정수_내림차순으로_정렬하기
+    {
+        public class Solution
+        {
+            public long solution(long n)
+            {
+                long answer = 0;
+                List<int> nums = new List<int>();
+                while(n >= 1)
+                {
+                    nums.Add((int)(n % 10));
+                    n /= 10;
+                }
+                nums.Sort();
+                nums.Reverse();
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    answer+= nums[i];
+                    if(i == nums.Count - 1)
+                        return answer;
+                    answer *= 10;
+                }
+                return answer;
+            }
+        }
+    }
+}
---
+++ CSharp_CodingTest/Programmers/lv_1/하샤드_수.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 하샤드_수
+{
+    public class Solution
+    {
+        public bool solution(int x)
+        {
+            int temp = 0;
+            int num = x;
+            while(x >= 1)
+            {
+                temp += (x % 10);
+                x /= 10;
+            }
+            if (num % temp == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
