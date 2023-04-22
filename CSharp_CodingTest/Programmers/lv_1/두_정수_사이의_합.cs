bd0763b8bfec2df806a2c5e6d6a97e7b199725c2
ssungho <thtl98@naver.com>
2023-04-22 23:08:46 +0900

    2023/04/22
    lv1 두 정수 사이의 합

---
+++ CSharp_CodingTest/Programmers/lv_1/두_정수_사이의_합.cs
@@ -0,0 +1,18 @@
+﻿namespace 두_정수_사이의_합
+{
+    public class Solution
+    {
+        public long solution(int a, int b)
+        {
+            long answer = 0;
+            if (a > b)
+                for (int i = b; i <= a; i++)
+                    answer += i;
+            else if (a < b)
+                for (int i = a; i <= b; i++)
+                    answer += i;
+            else return a;
+            return answer;
+        }
+    }
+}
