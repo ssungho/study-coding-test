9da8ebbb4a777dccfb7b696a7a1c033e21a8b729
ssungho <thtl98@naver.com>
2023-04-25 23:59:08 +0900

    2023/04/25
    lv1 서울에서 김서방 찾기,
    lv1 콜라츠 추측

---
+++ CSharp_CodingTest/Programmers/lv_1/서울에서_김서방_찾기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 서울에서_김서방_찾기
+{
+    public class Solution
+    {
+        public string solution(string[] seoul)
+        {
+            int x = 0;
+            foreach (string s in seoul) 
+            {
+                if (s == "Kim")
+                    return new string($"김서방은 {x}에 있다");
+                x++;
+            }
+            return null;
+        }
+    }
+}
---
+++ CSharp_CodingTest/Programmers/lv_1/콜라츠_추측.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 콜라츠_추측
+{
+    public class Solution
+    {
+        public int solution(int num)
+        {
+            int cnt = 0;
+            while (cnt < 500)
+            {
+                if (num == 1)
+                    return cnt;
+                if (num % 2 == 0)
+                    num = num / 2;
+                else if (num % 2 == 1)
+                    num = num * 3 + 1;
+                cnt++;
+            }
+            return -1;
+        }
+    }
+}
