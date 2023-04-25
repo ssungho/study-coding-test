563ee4ef7970fdb2630802070e23a7919be01e94
ssungho <thtl98@naver.com>
2023-04-26 02:23:25 +0900

    2023/04/26
    lv1 나누어 떨어지는 숫자 배열,
    lv1 모의고사

---
+++ CSharp_CodingTest/Programmers/lv_1/나누어_떨어지는_숫자_배열.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 나누어_떨어지는_숫자_배열
+{
+    public class Solution
+    {
+        public int[] solution(int[] arr, int divisor)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % divisor == 0)
+                    list.Add(arr[i]);
+            }
+
+            list.Sort();
+
+            if (list.Count == 0)
+                list.Add(-1);
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ CSharp_CodingTest/Programmers/lv_1/모의고사.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace 모의고사
+{
+    public class Solution
+    {
+        public int[] solution(int[] answers)
+        {
+            int[] scores = new int[3];
+
+            int[] s1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] s2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+            int[] s3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (answers[i] == s1[i % s1.Length])
+                    scores[0] += 1;
+                if (answers[i] == s2[i % s2.Length])
+                    scores[1] += 1;
+                if (answers[i] == s3[i % s3.Length])
+                    scores[2] += 1;
+            }
+
+            List<int> answer = new List<int>();
+            if ((scores[0] == scores.Max()))
+                answer.Add(1);
+            if ((scores[1] == scores.Max()))
+                answer.Add(2);
+            if ((scores[2] == scores.Max()))
+                answer.Add(3);
+            return answer.ToArray();
+        }
+    }
+}
