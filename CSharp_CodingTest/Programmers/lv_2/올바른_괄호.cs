039e3ec53dc926b43348a79c3545271f3aed5f3e
ssungho <thtl98@naver.com>
2023-05-01 16:54:02 +0900

    2023/05/01
    lv1 올바른 괄호

---
+++ CSharp_CodingTest/Programmers/lv_2/올바른_괄호.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 올바른_괄호
+{
+    public class Solution
+    {
+        public bool solution(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                    stack.Push(s[i]); // stack.push('('); 문자 바꿔서 효율성2 통과..;
+                else
+                {
+                    if(stack.Count == 0)
+                        return false;
+                    
+                    stack.Pop();
+                }
+            }
+
+            if (stack.Count > 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
