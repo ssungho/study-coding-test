7d1d21b83a857aa03e4497913dd6161d6f569937
ssungho <thtl98@naver.com>
2023-05-02 23:59:38 +0900

    2023/05/02
    lv1 제일 작은 수 제거하기

---
+++ CSharp_CodingTest/Programmers/lv_1/제일_작은_수_제거하기.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace 제일_작은_수_제거하기
+{
+    public class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr.Min())
+                    list.Add(arr[i]);
+            }
+            if (list.Count == 0)
+                list.Add(-1);
+            return list.ToArray();
+        }
+    }
+}
