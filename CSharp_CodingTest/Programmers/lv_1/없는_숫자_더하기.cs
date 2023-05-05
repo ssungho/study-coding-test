83306c2f5c1f5d73633c26c695fa1db8ccb35cc0
ssungho <thtl98@naver.com>
2023-05-05 22:31:54 +0900

    2023/05/04
    lv1 없는 숫자 더하기

---
+++ CSharp_CodingTest/Programmers/lv_1/없는_숫자_더하기.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 없는_숫자_더하기
+{
+    public class Solution
+    {
+        public int solution(int[] numbers)
+        {
+            int answer = 45;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer -= numbers[i];
+            }
+            return answer;
+        }
+    }
+}
