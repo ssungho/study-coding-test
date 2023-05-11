2d63fb80ceaf104f38003c1c2f9924045e21dd98
ssungho <thtl98@naver.com>
2023-05-12 01:11:43 +0900

    2023/05/12
    lv1 약수의 개수와 덧셈
    lv2 프로세스

---
+++ CSharp_CodingTest/Programmers/lv_1/약수의 개수와 덧셈.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 약수의_개수와_덧셈
+{
+    public class Solution
+    {
+        public int solution(int left, int right)
+        {
+            int answer = 0;
+            for (int i = left; i <= right; i++)
+            {
+                int count = 0;
+
+                for (int j = 1; j <= i; j++)
+                    if (i % j == 0)
+                        count++;
+
+                if (count % 2 == 0)
+                    answer += i;
+
+                else if (count % 2 == 1)
+                    answer -= i;
+
+            }
+            return answer;
+        }
+    }
+}
