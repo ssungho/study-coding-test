097b2b6e04ea35ce1c666db613981709f2dd37a1
ssungho <thtl98@naver.com>
2023-05-13 02:14:15 +0900

    2023/05/13
    lv1 문자열 다루기 기본.
    lv1 부족한 금액 계산하기.

---
+++ CSharp_CodingTest/Programmers/lv_1/문자열 다루기 기본.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace 문자열_다루기_기본
+{
+    public class Solution
+    {
+        public bool solution(string s)
+        {
+            char[] num = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            if (s.Length == 4 || s.Length == 6)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!num.Contains(s[i]))
+                        return false;
+                }
+            }
+            else
+                return false;
+            return true;
+        }
+    }
+}
---
+++ CSharp_CodingTest/Programmers/lv_1/부족한 금액 계산하기.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 부족한_금액_계산하기
+{
+    class Solution
+    {
+        public long solution(int price, long money, int count)
+        {
+            int N = 1;
+            while (N <= count)
+            {
+                money -= price * N;
+                N++;
+            }
+            if (money < 0)
+                return Math.Abs(money);
+            else
+                return 0;
+        }
+    }
+}
