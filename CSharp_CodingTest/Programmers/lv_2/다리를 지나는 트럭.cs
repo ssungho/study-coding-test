3b2111036781ce5d88e4fdee0626474ecdcfe7cb
ssungho <thtl98@naver.com>
2023-05-15 00:33:16 +0900

    2023/05/15
    lv2 다리를 지나는 트럭

---
+++ CSharp_CodingTest/Programmers/lv_2/다리를 지나는 트럭.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace 다리를_지나는_트럭
+{
+    class Solution
+    {
+        public int solution(int bridge_length, int weight, int[] truck_weights)
+        {
+            int totalWeight = 0;
+            int time = 0;
+            int truck = 0;
+            int move = 0;
+            Queue<int> queue = new Queue<int>();
+
+            while (truck < truck_weights.Length)
+            {
+                if (queue.Count == bridge_length)
+                    totalWeight -= queue.Dequeue();
+
+                if (totalWeight + truck_weights[truck] <= weight)
+                {
+                    totalWeight += truck_weights[truck];
+                    queue.Enqueue(truck_weights[truck]);
+                    truck++;
+                }
+
+                else
+                    queue.Enqueue(move);
+
+                time++;
+            }
+
+            return time + bridge_length;
+        }
+    }
+}
