7786dbf05731332abbc6177b379eb40c7509f803
ssungho <thtl98@naver.com>
2023-05-14 23:57:20 +0900

    2023/05/14
    lv1 직사각형 별찍기
    lv1 최대공약수와 최소공배수
    lv1 행렬의 덧셈

---
+++ CSharp_CodingTest/Programmers/lv_1/최대공약수와 최소공배수.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 최대공약수와_최소공배수
+{
+    public class Solution
+    {
+        public int[] solution(int n, int m)
+        {
+            int[] answer = new int[2];
+            
+            for (int i = 1; i <= MathF.Min(n,m); i++)
+                if(n % i == 0 && m % i == 0)
+                    answer[0] = i;
+                
+            answer[1] = (n * m) / answer[0];
+
+            return answer;
+        }
+    }
+}
