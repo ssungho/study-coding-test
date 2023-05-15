cb4fae542cd7f0834a3b1e3c88389b4f49194157
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-05-16 00:32:35 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(void) 
+{
+	int N;
+	
+	cin >> N;
+
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = i; j < N - 1; j++) 
+			cout << " ";
+		
+		for (int j = 0; j < i + 1; j++)
+			cout << "*";
+		
+		cout << '\n';
+	}
+	
+	return 0;
+}
