16240e05ac96fc6e4eb5680fb99cfd63c89601d2
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-05-16 09:55:15 +0900

    [level 2] Title: 프로세스, Time: 3.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] priorities, int location)
+    {
+        int answer = 0;
+        Queue<(int, bool)>q = new Queue<(int, bool)>();
+
+        for (int i = 0; i < priorities.Length; i++)
+            if (i == location)
+                q.Enqueue((priorities[i], true));
+
+            else
+                q.Enqueue((priorities[i], false));
+
+        while (q.Count > 0)
+        {
+            if (q.Peek().Item2 == true && q.Peek().Item1 == q.Max().Item1)
+            {
+                answer++;
+                return answer;
+            }
+            if (q.Peek().Item1 == q.Max().Item1)
+            {
+                q.Dequeue();
+                answer++;
+            }
+            else if(q.Peek().Item1 != q.Max().Item1)
+                q.Enqueue(q.Dequeue());
+            
+        }
+        return answer;
+    }
+}
