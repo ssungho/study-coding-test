fb6b164640992acc89e68776517311614e0025d6
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-05-29 23:47:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 720 ms, Memory: 8180 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int n, m;
+
+	cin >> n;
+	
+	vector<int> v1;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v1.push_back(temp);
+	}
+	
+	std::sort(v1.begin(), v1.end());
+
+	cin >> m;
+
+	vector<int> v2;
+	for (int i = 0; i < m; i++)
+	{
+		int temp;
+		cin >> temp;
+		v2.push_back(temp);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int cnt = 0;
+		cnt = upper_bound(v1.begin(), v1.end(), v2[i]) - lower_bound(v1.begin(), v1.end(), v2[i]);
+		cout << cnt << " ";
+	}
+
+	return 0;
+}
