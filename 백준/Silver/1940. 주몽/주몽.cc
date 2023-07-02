7aadf4c2ef7b03e8f748fe3ed914b85e2c0de80f
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-07-03 03:13:46 +0900

    [Silver IV] Title: 주몽, Time: 8 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(void)
+{
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+
+	int start = 0;
+	int end = N - 1;
+	int cnt = 0;
+	while (start != end)
+	{
+		if (v[start] + v[end] == M)
+		{
+			cnt++;
+			end--;
+		}
+		else if (v[start] + v[end] > M)
+			end--;
+
+		else if (v[start] + v[end] < M)
+			start++;
+	}
+	cout << cnt;
+	return 0;
+}
