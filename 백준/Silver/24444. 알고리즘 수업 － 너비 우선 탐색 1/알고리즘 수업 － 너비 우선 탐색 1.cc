7381b2ccd0c323dce26d0c20c658383f39d6f0a8
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-07-03 23:12:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 84 ms, Memory: 9552 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+vector<int> graph[100001];
+int visited[100001];
+
+void BFS(int start_node)
+{
+	int seq = 1;
+	queue<int> q;
+
+	q.push(start_node);
+	visited[start_node] = seq;
+	seq++;
+
+	while (!q.empty())
+	{
+		int cur_node = q.front();
+		q.pop();
+		for (int i = 0; i < graph[cur_node].size(); i++)
+		{
+			// 방문하지 않았다면,
+			if (visited[graph[cur_node][i]] == 0)
+			{
+				visited[graph[cur_node][i]] = seq;
+				seq++;
+				q.push(graph[cur_node][i]);
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	int node1, node2;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> node1 >> node2;
+		graph[node1].push_back(node2);
+		graph[node2].push_back(node1);
+	}
+	for (int i = 1; i < N + 1; i++)
+		sort(graph[i].begin(), graph[i].end());
+	
+	BFS(R);
+	for (int i = 1; i < N + 1; i++)
+		cout << visited[i] << '\n';
+	
+	return 0;
+}
