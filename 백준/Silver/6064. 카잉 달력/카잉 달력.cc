1df691b27ebd93a3561a768b4e5d49a030e635e1
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2023-07-07 20:24:14 +0900

    [Silver I] Title: 카잉 달력, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int M, N, x, y;
+		cin >> M >> N >> x >> y;
+		int ny = 0, k = 0, cnt = 0;
+		while (true)
+		{
+			k = (M * cnt) + x;
+			ny = k % N;
+			if (ny == 0)
+				ny = N;
+			if (ny == y)
+			{
+				cout << k << '\n';
+				break;
+			}
+			else
+				cnt++;
+			if (k > M * N)
+			{
+				cout << -1 << '\n';
+				break;
+			}
+		}
+	}
+	return 0;
+}
