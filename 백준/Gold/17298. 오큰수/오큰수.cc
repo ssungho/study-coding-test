8246847cd984c0bcdd4cddd4d2c95934de6b37de
SSungHo <103364628+ssungho@users.noreply.github.com>
2023-07-25 01:35:41 +0900

    [Gold IV] Title: 오큰수, Time: 620 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main(void)
+{
+	int N;
+	cin >> N;
+	
+	vector<int> answer(N + 1); // 정답 배열
+	vector<int> v(N + 1); // 입력 받을 배열
+	stack<int> s; // 스택
+
+	for (int i = 1; i < N + 1; i++)
+	{
+		int temp;
+		cin >> temp;
+		v[i] = temp;
+	}
+
+	for (int i = 1; i < N + 1; i++)
+	{
+		while (!s.empty() && s.top() < i && v[s.top()] < v[i])
+		{
+			answer[s.top()] = v[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+
+	while (!s.empty())
+	{
+		answer[s.top()] = -1;
+		s.pop();
+	}
+
+	for (int i = 1; i < N + 1; i++)
+	{
+		cout << answer[i] << " ";
+	}
+
+	return 0;
+}
