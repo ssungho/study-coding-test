dcd733170ad5266de3fd137a6fbd4831fab6b8d8
SSungHo <103364628+ssungho@users.noreply.github.com>
2023-07-26 20:41:01 +0900

    [Silver II] Title: 이동하기, Time: 228 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int miro[1001][1001];
+int memo_table[1001][1001];
+int n, m; // 행, 열
+
+int func(int x, int y)
+{
+	if (x == 1 && y == 1)
+		return memo_table[1][1] = miro[1][1];
+
+	memo_table[x][y] 
+	= max({ memo_table[x - 1][y - 1] + miro[x][y],
+			memo_table[x - 1][y]+ miro[x][y],
+			memo_table[x][y - 1] + miro[x][y] });
+
+	return memo_table[x][y];
+}
+
+int main(void)
+{
+	int candy;
+	cin >> n >> m;
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 1; j < m + 1; j++)
+		{
+			cin >> candy;
+			miro[i][j] = candy;
+		}
+	}
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 1; j < m + 1; j++)
+		{
+			func(i, j);
+		}
+	}
+	cout << memo_table[n][m];
+	return 0;
+}
