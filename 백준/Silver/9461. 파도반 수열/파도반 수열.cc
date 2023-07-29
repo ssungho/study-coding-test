635ea794da0fad79dd54ca6cdd05f70c7a8c0e6a
SSungHo <103364628+ssungho@users.noreply.github.com>
2023-07-29 20:49:52 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -5,12 +5,11 @@ long long memo[101];
 long long FindP(int x)
 {
 	memo[1] = 1, memo[2] = 1, memo[3] = 1;
-	memo[4] = 2, memo[5] = 2;
 
-	for (int i = 6; i < x + 1; i++)
+	for (int i = 4; i < x + 1; i++)
 	{
 		if(memo[i] == 0)
-			memo[i] = memo[i - 5] + memo[i - 1];
+			memo[i] = memo[i - 2] + memo[i - 3];
 	}
 
 	return memo[x];
