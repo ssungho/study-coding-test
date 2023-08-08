8b9107ff473440a0b20b977bc91fc7c6c6501cc2
SSungHo <103364628+ssungho@users.noreply.github.com>
2023-08-09 00:42:14 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -5,26 +5,10 @@ using namespace std;
 int main(void)
 {
     string temp;
-    getline(cin, temp);
     int cnt = 0;
-    if (temp == " ")
-    {
-        cout << cnt;
-    }
-    else
-    {
-        for (int i = 0; i < temp.size(); i++)
-        {
-            if (temp[i] == ' ')
-            {
-                if (i != 0)
-                    cnt++;
-            }
-            if (i == temp.size() - 1 && temp[i] != ' ')
-                cnt++;
-        }
-        cout << cnt;
-    }
-
+    while(cin >> temp)
+        cnt++;
+    
+    cout << cnt;
     return 0;
 }
