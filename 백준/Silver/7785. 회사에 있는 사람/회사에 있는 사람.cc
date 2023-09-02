b9b5444ed193f724de920402efd1c7bc3e461210
ssugho <103364628+ssungho@users.noreply.github.com>
2023-09-02 23:55:19 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 152 ms, Memory: 17092 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -7,6 +7,10 @@ using namespace std;
 
 int main(void)
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     map<string, string> log;
     vector<string> names;
     int N;
