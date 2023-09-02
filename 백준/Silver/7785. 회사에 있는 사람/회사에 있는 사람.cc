3e61b10c7be5a161a86375633938f42ff9600371
ssugho <103364628+ssungho@users.noreply.github.com>
2023-09-02 23:54:24 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 212 ms, Memory: 16960 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    map<string, string> log;
+    vector<string> names;
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++)
+    {
+        string name;
+        string state;
+        cin >> name >> state;
+        if(log.find(name) == log.end())
+        {
+            log.insert({name, state});
+            names.push_back(name);
+        }
+        else
+        {
+            log[name] = state;
+        }
+    }
+    sort(names.begin(), names.end(), greater<>());
+    
+    for(int i = 0; i < names.size(); i++)
+    {
+        string name = names[i];
+        if(log[name] == "enter")
+        {
+            cout << name << '\n';
+        }
+    }
+
+    return 0;
+}
