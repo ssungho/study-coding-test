70d26fcf3f2bc1edf9120049f88a4c24ffcb9d66
ssugho <103364628+ssungho@users.noreply.github.com>
2023-09-14 03:00:37 +0900

    [Silver I] Title: 그림, Time: 76 ms, Memory: 3248 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int graph[501][501];
+bool visited[501][501];
+int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
+
+int BFS(int y, int x)
+{
+    queue<pair<int, int>> q;
+    q.push({y, x});
+    visited[y][x] = true;
+    int area = 1;
+
+    while (!q.empty())
+    {
+        int curr_y = q.front().first;
+        int curr_x = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = curr_y + dy[i];
+            int nx = curr_x + dx[i];
+            if (ny > 0 && nx > 0 && ny < 501 && nx < 501 && !visited[ny][nx] && graph[ny][nx] == 1)
+            {
+                q.push({ny, nx});
+                visited[ny][nx] = true;
+                area++;
+            }
+        }
+    }
+    return area;
+}
+
+int main(void)
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < m + 1; j++)
+        {
+            int temp;
+            cin >> temp;
+            graph[i][j] = temp;
+        }
+    }
+
+    int count = 0;
+    int max_area = 0;
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < m + 1; j++)
+        {
+            if (graph[i][j] == 1 && !visited[i][j])
+            {
+                int area = BFS(i, j);
+                if (area > max_area)
+                    max_area = area;
+                count++;
+            }
+        }
+    }
+    cout << count << '\n' << max_area;
+
+    return 0;
+}
