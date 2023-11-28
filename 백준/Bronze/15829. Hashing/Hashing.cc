a5125252369650e322c6a9dfb25b571abe513ad1
ssugho <103364628+ssungho@users.noreply.github.com>
2023-11-28 15:12:12 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -10,16 +10,16 @@ int main(void)
 
 	long long sum = 0;
 	long long r = 1;
+	long long m = 1234567891;
 
 	for (int i = 0; i < l; i++)
 	{
 		char c = str[i];
+		long long ll = c + 1 - 'a';
 
-		long long ll = c - 'a' + 1;
 		ll *= r;
-		sum += ll;
-
-		r *= 31;
+		sum = (sum + ll) % m;
+		r = r * 31 % m;
 	}
 
 	cout << sum;
