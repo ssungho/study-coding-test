5748684f3e6218502c39e1bfe8399aa54692fc63
ssugho <103364628+ssungho@users.noreply.github.com>
2023-11-29 14:49:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int paper[129][129];
+int white = 0;
+int blue = 0;
+
+void Cut(int x, int y, int length)
+{
+	int count = 0;
+	for (int i = x; i < x + length; i++)
+	{
+		for (int j = y; j < y + length; j++)
+		{
+			if (paper[i][j])
+				count++;
+		}
+	}
+
+	if (count == 0)
+		white++;
+	else if (count == length * length)
+		blue++;
+	else
+	{
+		Cut(x, y, length / 2);
+		Cut(x + length / 2, y, length / 2);
+		Cut(x, y + length / 2, length / 2);
+		Cut(x + length / 2, y + length / 2, length / 2);
+	}
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 1; j < n + 1; j++)
+		{
+			int color = 0;
+			cin >> color;
+			paper[i][j] = color;
+		}
+	}
+
+	Cut(1, 1, n);
+	cout << white << " " << blue;
+
+	return 0;
+}
