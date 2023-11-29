908722e56918c1ff5f91205b634493ea107a3ef5
ssugho <103364628+ssungho@users.noreply.github.com>
2023-11-29 13:16:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 296 ms, Memory: 6400 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> nodes[1001];
+bool visited[1001];
+
+void DFS(int node)
+{
+	visited[node] = true;
+
+	for (int i = 0; i < nodes[node].size(); i++)
+	{
+		int next_node = nodes[node][i];
+		if (!visited[next_node])
+			DFS(next_node);
+	}
+
+	return;
+}
+
+int main(void)
+{
+	int node, edge;
+	cin >> node >> edge;
+
+	for (int i = 0; i < edge; i++)
+	{
+		int node1, node2;
+		cin >> node1 >> node2;
+
+		nodes[node1].push_back(node2);
+		nodes[node2].push_back(node1);
+	}
+
+	int count = 0;
+	for (int i = 1; i < node + 1; i++)
+	{
+		if (!visited[i])
+		{
+			DFS(i);
+			count++;
+		}
+	}
+
+	cout << count;
+
+	return 0;
+}
