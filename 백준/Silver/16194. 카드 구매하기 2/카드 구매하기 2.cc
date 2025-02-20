0474cde6095775e3c1a63a1dd61e397053ec3380
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-02-20 22:03:43 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int price[1001];
+int memo[1001];
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> price[i];
+    }
+
+    memo[1] = price[1];
+
+    for (int i = 2; i <= N; i++)
+    {
+        memo[i] = price[i];
+        for (int j = 1; j < i; j++)
+        {
+            memo[i] = min(memo[i], memo[j] + memo[i - j]);
+        }
+    }
+
+    cout << memo[N];
+
+    return 0;
+}
