172c5d1d9b5e9d96ce52d62304062dce0def046a
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-02-24 09:40:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 288 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,24 +1,83 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
 
-int main(void)
+using namespace std;
+
+vector<int> v;
+vector<int> tmp;
+
+void Sort(int start, int end)
 {
-	using namespace std;
+    if (end - start < 1)
+        return;
 
-	int N;
-	cin >> N;
-	
-	int *nums = new int[N];
+    int mid = start + (end - start) / 2;
 
-	for (int i = 0; i < N; i++)
-	{
-		cin >> nums[i];
-	}
+    Sort(start, mid);
+    Sort(mid + 1, end);
 
-	sort(nums, nums + N);
-	 
-	for (int i = 0; i < N; i++)
-	{
-		cout << nums[i] << '\n';
-	}
+    // 임시 배열에 값 저장.
+    for (int i = start; i <= end; i++)
+    {
+        tmp[i] = v[i];
+    }
+
+    int k = start;
+    int index1 = start;
+    int index2 = mid + 1;
+
+    while (index1 <= mid && index2 <= end)
+    {
+        if (tmp[index1] > tmp[index2])
+        {
+            v[k] = tmp[index2];
+            k++;
+            index2++;
+        }
+        else
+        {
+            v[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+    }
+
+    while (index1 <= mid)
+    {
+        v[k] = tmp[index1];
+        k++;
+        index1++;
+    }
+
+    while (index2 <= mid)
+    {
+        v[k] = tmp[index2];
+        k++;
+        index2++;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    v = vector<int>(N + 1, 0);
+    tmp = vector<int>(N + 1, 0);
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> v[i];
+    }
+
+    Sort(1, N);
+
+    for (int i = 1; i <= N; i++)
+    {
+        cout << v[i] << '\n';
+    }
 }
