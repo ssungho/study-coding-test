182a7344f9465a5d78828a6fabbfc80cfc7f4437
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-02-26 14:35:16 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    // 값, 자리
+    vector<pair<int, int>> v;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int value;
+        cin >> value;
+        v.push_back({value, i});
+    }
+
+    sort(v.begin(), v.end(), [](auto &a, auto &b)
+         { return a.first < b.first; });
+
+    int x;
+    cin >> x;
+
+    int start = 0;
+    int end = n - 1;
+    int count = 0;
+
+    while (start < end)
+    {
+        int target = v[start].first + v[end].first;
+
+        if (target == x)
+        {
+            count++;
+
+            if (abs(x - v[start + 1].first) <= abs(x - v[end - 1].first))
+                start++;
+            else
+                end--;
+        }
+        else if (target > x)
+            end--;
+        else
+            start++;
+    }
+
+    cout << count;
+
+    return 0;
+}
