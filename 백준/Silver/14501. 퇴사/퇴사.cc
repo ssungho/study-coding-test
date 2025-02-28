a29364a7110d522b3ddc4e3afe62c85a7e5668d8
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-02-28 20:38:07 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -8,39 +8,25 @@ int main(void)
     int N;
     cin >> N;
 
-    vector<pair<int, int>> v(N + 1);
+    // 5일이 최대. 15 + 5 = 20
+    int T[20]{};
+    int P[20]{};
+    int memo[20]{};
 
     for (int i = 1; i <= N; i++)
     {
-        cin >> v[i].first >> v[i].second;
+        cin >> T[i] >> P[i];
     }
-
-    vector<int> memo(N + 1, 0);
-    int max_pay = 0;
 
     for (int i = N; i > 0; i--)
-    {
-        if (v[i].first + i - 1 <= N)
-        {
-            int next = i + v[i].first;
-            int next_pay;
-
-            if (next > N)
-                next_pay = 0;
-            else
-                next_pay = memo[i + v[i].first];
-
-            memo[i] = max(max_pay, next_pay + v[i].second);
-        }
+     {
+        if (T[i] + i -1 <= N) 
+            memo[i] = max(P[i] + memo[i + T[i]], memo[i + 1]);
         else
-        {
-            memo[i] = max_pay;
-        }
-        
-        max_pay = max(max_pay, memo[i]);
+            memo[i] = memo[i + 1];
     }
 
-    cout << max_pay;
-
+    cout << memo[1];
+    
     return 0;
 }
