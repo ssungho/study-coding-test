2a7a235d0f5b4e028bf705bfd9ab9c1e3fecc9a1
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-03-01 15:03:22 +0900

    [Silver I] Title: 스티커, Time: 256 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory.h>
+using namespace std;
+
+int stickers[2][100001]{};
+int memo[2][100001]{};
+
+int Test()
+{
+    memset(stickers, 0, sizeof(int) * 100001 * 2);
+    memset(memo, 0, sizeof(int) * 100001 * 2);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> stickers[i][j];
+        }
+    }
+
+    if (n == 1)
+        return max(stickers[0][1], stickers[1][1]);
+
+    memo[0][1] = stickers[0][1];
+    memo[1][1] = stickers[1][1];
+    memo[0][2] = stickers[0][2] + memo[1][1];
+    memo[1][2] = stickers[1][2] + memo[0][1];
+
+
+    for (int i = 3; i <= n; i++)
+    {
+        memo[0][i] = max(memo[1][i - 2], memo[1][i - 1]) + stickers[0][i];
+        memo[1][i] = max(memo[0][i - 2], memo[0][i - 1]) + stickers[1][i];
+    }
+
+    return max(memo[0][n], memo[1][n]);
+}
+
+int main(void)
+{
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cout << Test() << endl;
+    }
+
+    return 0;
+}
