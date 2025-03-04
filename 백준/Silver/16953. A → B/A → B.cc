761e3494bb3afc228205e1d8490c97dd7adf7eeb
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-03-04 16:49:01 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+long long Find(long long number, long long target, int count)
+{
+    if (number > target)
+    {
+        return INT_MAX;
+    }
+
+    if (number == target)
+    {
+        return count;
+    }
+
+    return min(Find(number * 2, target, count + 1), Find(number * 10 + 1, target, count + 1));
+}
+
+int main(void)
+{
+    int A, B;
+    cin >> A >> B;
+
+    long long count = Find(A, B, 1);
+
+    if (count == INT_MAX) count = -1;
+
+    cout << count;
+
+    return 0;
+}
