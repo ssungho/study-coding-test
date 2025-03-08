b6a501291a3ef46632a36d2c80bc48c40f738094
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-03-08 16:57:41 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int dy[4]{1, -1, 0, 0};
+int dx[4]{0, 0, -1, 1};
+
+using Lost_YX = pair<int, pair<int, int>>;
+
+int main(void)
+{
+    int test = 0;
+    while (true)
+    {
+        int N;
+        cin >> N;
+
+        if (N == 0)
+            break;
+
+        test++;
+
+        vector<vector<int>> graph(N, vector<int>(N));
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                cin >> graph[i][j];
+            }
+        }
+
+        priority_queue<Lost_YX, vector<Lost_YX>, greater<Lost_YX>> pq;
+
+        pq.push({graph[0][0], {0, 0}});
+
+        vector<vector<int>> lost_rupees(N, vector<int>(N, INT_MAX));
+
+        while (!pq.empty())
+        {
+            Lost_YX lost_yx = pq.top();
+            int rupee = lost_yx.first;
+            int y = lost_yx.second.first;
+            int x = lost_yx.second.second;
+            pq.pop();
+
+            if (rupee > lost_rupees[y][x])
+                continue;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny >= 0 && ny < N && nx >= 0 && nx < N)
+                {
+                    if (lost_rupees[ny][nx] > graph[ny][nx] + rupee)
+                    {
+                        lost_rupees[ny][nx] = graph[ny][nx] + rupee;
+                        pq.push({lost_rupees[ny][nx], {ny, nx}});
+                    }
+                }
+            }
+        }
+
+        cout << "Problem " << test << ": " << lost_rupees[N - 1][N - 1] << '\n';
+    }
+
+    return 0;
+}
