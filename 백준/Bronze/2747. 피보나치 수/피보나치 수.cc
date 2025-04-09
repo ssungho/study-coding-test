bdca52a2f027febe5db5760bb1b76c4a3a017691
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-04-09 21:19:28 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    int dp[46]{0, 1,};
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
