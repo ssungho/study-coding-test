a43bee32e5454e765cca745392a20ad002fc79ff
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-04-17 16:12:50 +0900

    [Gold IV] Title: 운동, Time: 232 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main(void)
+{
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<int>> dist(V + 1, vector<int>(V + 1, INT_MAX));
+
+    for (int i = 0; i < E; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        dist[a][b] = min(dist[a][b], c); 
+    }
+
+    for (int i = 1; i <= V; i++) 
+    {
+        dist[i][i] = 0;
+    }
+
+    for (int k = 1; k <= V; k++)
+    {
+        for (int i = 1; i <= V; i++)
+        {
+            for (int j = 1; j <= V; j++)
+            {
+                if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+                {
+                    continue;
+                }
+                else
+                {
+                    dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j]);
+                }
+            }
+        }
+    }
+
+    int min_cycle = INT_MAX;
+    for (int i = 1; i <= V; i++)
+    {
+        for (int j = 1; j <= V; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            if (dist[i][j] != INT_MAX && dist[j][i] != INT_MAX)
+            {
+                min_cycle = min(min_cycle, dist[i][j] + dist[j][i]);
+            }
+        }
+    }
+
+    min_cycle = min_cycle == INT_MAX ? -1 : min_cycle;
+
+    cout << min_cycle;
+
+    return 0;
+}
