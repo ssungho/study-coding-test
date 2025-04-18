0dac85a2af8bbed67a2d3879ec294c9db471f9b4
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-04-18 14:01:54 +0900

    [Gold IV] Title: 인구 이동, Time: 792 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+constexpr int DIR_SIZE{4};
+int dy[DIR_SIZE]{1, -1, 0, 0};
+int dx[DIR_SIZE]{0, 0, -1, 1};
+int N, L, R;
+
+bool BFS(vector<vector<int>>& graph, vector<vector<bool>>& visited, int start_y, int start_x) {
+   
+    queue<pair<int, int>> unions;
+    queue<pair<int, int>> q;
+    
+    unions.push({start_y, start_x});
+    q.push({start_y, start_x});
+    visited[start_y][start_x] = true;
+
+    int total = graph[start_y][start_x];
+
+    while (!q.empty()) {
+        pair<int, int> front = q.front();
+        int current_y = front.first;
+        int current_x = front.second;
+        q.pop();
+
+        for (int i = 0; i < DIR_SIZE; i++) {
+            int ny = dy[i] + current_y;
+            int nx = dx[i] + current_x;
+
+            if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx]) {
+                int diff = abs(graph[current_y][current_x] - graph[ny][nx]);
+                if (L <= diff && diff <= R) {
+                    q.push({ny, nx});
+                    unions.push({ny, nx});
+                    total += graph[ny][nx];
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+    }
+
+    if (unions.size() == 1) {
+        return false;
+    }
+
+    int average = total / (int)unions.size();
+
+    while (!unions.empty()) {
+        pair<int, int> pos_yx = unions.front();
+        unions.pop();
+        graph[pos_yx.first][pos_yx.second] = average;
+    }
+
+    return true;
+}
+
+int main(void) {
+    cin >> N >> L >> R;
+
+    vector<vector<int>> graph(N, vector<int>(N, 0));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    vector<vector<bool>> visited(N, vector<bool>(N, false));
+
+    int cnt = 0;
+    while (true) {
+        bool change = false;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    if (BFS(graph, visited, i, j)) {
+                        change = true;
+                    }
+                }
+            }
+        }
+
+        if (!change) {
+            break;
+        }
+
+        cnt++;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
