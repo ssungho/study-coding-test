d3ff4ebd19615ed5ccc8adacb1473bba685192e4
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-04-21 19:48:35 +0900

    [Silver I] Title: 신입 사원, Time: 1472 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+
+        vector<pair<int, int>> v(N);
+        for (int i = 0; i < N; i++)
+        {
+            cin >> v[i].first >> v[i].second;
+        }
+
+        sort(v.begin(), v.end());
+
+        int compare = v[0].second;
+        int count = 1;
+
+        for (int i = 1; i < N; i++)
+        {
+            if (v[i].second < compare)
+            {
+                count++;
+                compare = v[i].second;
+            }
+        }
+
+        cout << count << '\n';
+    }
+
+    return 0;
+}
