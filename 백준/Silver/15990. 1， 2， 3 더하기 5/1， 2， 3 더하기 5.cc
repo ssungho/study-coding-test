f0469a250c9db7333b07e60d1cc9a698b74a925f
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-05-12 18:39:00 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 0 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -5,10 +5,14 @@ using namespace std;
 
 constexpr long long LL_MOD{1000000009};
 constexpr int MAX_N{100001};
-long long dp[MAX_N][3] = {};
+long long dp[MAX_N][3]{};
 
 int main(void)
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     int T;
     cin >> T;
 
@@ -18,18 +22,17 @@ int main(void)
     dp[3][1] = 1;
     dp[3][2] = 1;
 
+    for (int i = 4; i < MAX_N; i++)
+    {
+        dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % LL_MOD;
+        dp[i][1] = (dp[i - 2][0] + dp[i - 2][2]) % LL_MOD;
+        dp[i][2] = (dp[i - 3][0] + dp[i - 3][1]) % LL_MOD;
+    }
+
     while (T--)
     {
         int n;
         cin >> n;
-
-        for (int i = 4; i <= n; i++)
-        {
-            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % LL_MOD;
-            dp[i][1] = (dp[i - 2][0] + dp[i - 2][2]) % LL_MOD;
-            dp[i][2] = (dp[i - 3][0] + dp[i - 3][1]) % LL_MOD;
-        }
-
         cout << (dp[n][0] + dp[n][1] + dp[n][2]) % LL_MOD << '\n';
     }
 
