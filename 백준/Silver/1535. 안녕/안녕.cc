0215851c8b70cddcb4b8a7173611302cb19406a9
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-05-14 17:09:31 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -2,47 +2,30 @@
 
 using namespace std;
 
-int N = 0;
-int max_joy = 0;
-
-void Tracking(int *L, int *J, bool *visited, int index, int life, int joy)
-{
-    max_joy = max(max_joy, joy);
-
-    for (int i = index; i < N; i++)
-    {
-        int next_life = life - L[i];
-
-        if (next_life > 0 && !visited[i])
-        {
-            visited[i] = true;
-            Tracking(L, J, visited, i + 1, next_life, joy + J[i]);
-            visited[i] = false;
-        }
-    }
-}
+constexpr int MAX_N{20};
+constexpr int MAX_HP{100};
 
 int main(void)
 {
+    int L[MAX_N]{};
+    int J[MAX_N]{};
+    int dp[MAX_HP + 1]{};
+
+    int N;
     cin >> N;
 
-    int L[20]{};
-    int J[20]{};
-    bool visited[20]{};
-
     for (int i = 0; i < N; i++)
-    {
         cin >> L[i];
-    }
+    for (int i = 0; i < N; i++)
+        cin >> J[i];
 
     for (int i = 0; i < N; i++)
     {
-        cin >> J[i];
+        for (int j = MAX_HP; j - L[i] > 0; j--)
+            dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
     }
 
-    Tracking(L, J, visited, 0, 100, 0);
-
-    cout << max_joy;
+    cout << dp[100];
 
     return 0;
 }
