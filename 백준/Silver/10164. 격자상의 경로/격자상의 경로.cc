b0f9c4a65732f0d9c98928a14ab3c45b791c0136
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-05-16 20:38:06 +0900

    [Silver II] Title: 격자상의 경로, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -11,47 +11,55 @@ int main(void)
     cin >> N >> M >> K;
 
     int answer = 0;
-    int dp[MAX_N][MAX_M]{};
+    int dp1[MAX_N][MAX_M]{};
 
     for (int i = 1; i <= N; i++)
     {
-        dp[i][1] = 1;
+        dp1[i][1] = 1;
     }
-    
+
     for (int i = 1; i <= M; i++)
     {
-        dp[1][i] = 1;
+        dp1[1][i] = 1;
     }
 
-    int start_i = 2;
-    int start_j = 2;
-
-    for (int i = start_i; i <= N; i++)
+    for (int i = 2; i <= N; i++)
     {
-        for (int j = start_j; j <= M; j++)
+        for (int j = 2; j <= M; j++)
         {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-
-            if (M * (i - 1) + j == K)
-            {
-                for (int k = i; k <= N; k++)
-                {
-                    dp[k][j] = dp[i][j];
-                }
-
-                for (int k = j; k <= M; k++)
-                {
-                    dp[i][k] = dp[i][j];
-                }
-
-                start_i = i;
-                start_j = j;
-                i++;
-            }
+            dp1[i][j] = dp1[i - 1][j] + dp1[i][j - 1];
         }
     }
 
-    cout << dp[N][M];
+    if (K != 0)
+    {
+        int dp2[MAX_N][MAX_M]{};
+
+        int start_i = (K - 1) / M + 1;
+        int start_j = (K - 1) % M + 1;
+
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= M; j++)
+            {
+                dp2[i][j] = 1;
+            }
+        }
+
+        for (int i = start_i + 1; i <= N; i++)
+        {
+            for (int j = start_j + 1; j <= M; j++)
+            {
+                dp2[i][j] = dp2[i - 1][j] + dp2[i][j - 1];
+            }
+        }
+        
+        cout << dp1[start_i][start_j] * dp2[N][M];
+    }
+    else
+    {
+        cout << dp1[N][M];
+    }
 
     return 0;
 }
