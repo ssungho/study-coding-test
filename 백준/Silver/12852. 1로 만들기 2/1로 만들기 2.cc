924849e2821533b9d258e3d9ef8c4f61ee95520f
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-05-21 16:05:28 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    if (N == 1)
+    {
+        cout << "0\n1";
+        return 0;
+    }
+
+    vector<int> dp(N + 1, INT_MAX);
+    vector<int> path(N + 1, 0);
+    dp[N] = 0;
+
+    for (int i = N; i > 0; i--)
+    {
+        int prev = 0;
+        if (i % 3 == 0)
+        {
+            if (dp[i / 3] > dp[i] + 1)
+            {
+                dp[i / 3] = dp[i] + 1;
+                path[i / 3] = i;
+            }
+        }
+
+        if (i % 2 == 0)
+        {
+            if (dp[i / 2] > dp[i] + 1)
+            {
+                dp[i / 2] = dp[i] + 1;
+                path[i / 2] = i;
+            }
+        }
+
+        if (dp[i - 1] > dp[i] + 1)
+        {
+            dp[i - 1] = dp[i] + 1;
+            path[i - 1] = i;
+        }
+    }
+
+    cout << dp[1] << '\n';
+
+    stack<int> result;
+    int current = 1;
+    result.push(current);
+
+    while (true)
+    {
+        result.push(path[current]);
+        current = path[current];
+        if (current == N)
+        {
+            break;
+        }
+    }
+
+    while (!result.empty())
+    {
+        cout << result.top() << " ";
+        result.pop();
+    }
+
+    return 0;
+}
