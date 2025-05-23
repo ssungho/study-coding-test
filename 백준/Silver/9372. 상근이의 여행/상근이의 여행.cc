612b30e1407516e2c25d92373faa69af454c28bf
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-05-23 12:46:37 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,32 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
 
 using namespace std;
-
-int Find(vector<int> &parent, int node)
-{
-    if (parent[node] == node)
-        return node;
-    return parent[node] = Find(parent, parent[node]);
-}
-
-bool Union(vector<int> &parent, int node1, int node2)
-{
-    int root1 = Find(parent, node1);
-    int root2 = Find(parent, node2);
-    
-    if (root1 == root2)
-        return false;
-
-    if (root1 < root2)
-        parent[root1] = root2;
-    else
-        parent[root2] = root1;
-
-    return true;
-}
 
 int main(void)
 {
@@ -37,44 +11,14 @@ int main(void)
     {
         int N, M;
         cin >> N >> M;
-        vector<vector<int>> graph(N + 1);
 
-        int start;
         for (int i = 0; i < M; i++)
         {
             int a, b;
             cin >> a >> b;
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-            start = a;
         }
 
-        vector<int> parent(N + 1, 0);
-        for (int i = 1; i <= N; i++)
-        {
-            parent[i] = i;
-        }
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({1, start});
-        int result = 0;
-        while (!pq.empty())
-        {
-            int cost = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            for (auto next : graph[node])
-            {
-                if (Union(parent, node, next))
-                {
-                    result++;
-                    pq.push({cost + 1, next});
-                }
-            }
-        }
-
-        cout << result << '\n';
+        cout << N - 1 << '\n';
     }
 
     return 0;
