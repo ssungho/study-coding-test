dc858c9794f8b41ad926a6ddb70cf553f0d3f446
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-07-11 11:31:02 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -5,119 +5,77 @@
 
 using namespace std;
 
+constexpr int dir_size{4};
+constexpr int arr_size{8};
+constexpr int dy[dir_size]{-1, 0, 1, 0};
+constexpr int dx[dir_size]{0, 1, 0, -1};
+
 int N, M;
+int lab[arr_size][arr_size] {};
+int temp[arr_size][arr_size] {};
+bool visited[arr_size][arr_size] {};
+vector<pair<int, int>> safe_zones;
+vector<pair<int, int>> virus_zones;
 
-int map[8][8]{};
-int test_map[8][8]{};
-bool visited[8][8]{};
-
-int dy[4]{1, -1, 0, 0};
-int dx[4]{0, 0, -1, 1};
-
-vector<pair<int, int>> safe_area;
-vector<pair<int, int>> virus_area;
-
-void BFS(int y, int x)
-{
-    queue<pair<int, int>> q;
-    q.push({y, x});
-    visited[y][x] = true;
-
-    while (!q.empty())
-    {
-        pair<int, int> front = q.front();
-        int current_y = front.first;
-        int current_x = front.second;
-        q.pop();
-
-        for (int i = 0; i < 4; i++)
-        {
-            int next_y = current_y + dy[i];
-            int next_x = current_x + dx[i];
-
-            if (next_y >= 0 && next_y < N && next_x >= 0 && next_x < M &&
-                test_map[next_y][next_x] == 0 && !visited[next_y][next_x])
-            {
-                q.push({next_y, next_x});
-                visited[next_y][next_x] = true;
-                test_map[next_y][next_x] = 2;
-            }
-        }
-    }
-}
-
-int CheckSafeArea()
-{
-    int count = 0;
-
-    for (int y = 0; y < N; y++)
-        for (int x = 0; x < M; x++)
-            if (test_map[y][x] == 0)
-                count++;
-
-    return count;
-}
-
-int main(void)
-{
+int main(void) {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     cin >> N >> M;
-
-    for (int y = 0; y < N; y++)
-    {
-        for (int x = 0; x < M; x++)
-        {
-            cin >> map[y][x];
-
-            if (map[y][x] == 0)
-                safe_area.push_back({y, x});
-            else if (map[y][x] == 2)
-                virus_area.push_back({y, x});
-        }
-    }
-
-    int max_area = -1;
-
-    int safe_area_size = safe_area.size();
-
-    for (int i = 0; i < safe_area_size; i++)
-    {
-        for (int j = 0; j < safe_area_size; j++)
-        {
-            for (int k = 0; k < safe_area_size; k++)
-            {
-                if (i == j || i == k || j == k)
-                    continue;
-
-                memset(visited, 0, sizeof(bool) * 64);
-                memset(test_map, 0, sizeof(int) * 64);
-
-                memcpy(test_map, map, sizeof(int) * 64);
-
-                pair<int, int> wall_pos = safe_area[i];
-                test_map[wall_pos.first][wall_pos.second] = 1;
-
-                wall_pos = safe_area[j];
-                test_map[wall_pos.first][wall_pos.second] = 1;
-
-                wall_pos = safe_area[k];
-                test_map[wall_pos.first][wall_pos.second] = 1;
-
-                int virus_count = virus_area.size();
-
-                for (int h = 0; h < virus_count; h++)
-                {
-                    BFS(virus_area[h].first, virus_area[h].second);
-                }
-
-                max_area = max(CheckSafeArea(), max_area);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> lab[i][j];
+            if (lab[i][j] == 0) {
+                safe_zones.push_back({i, j});
+            }
+            else if (lab[i][j] == 2) {
+                virus_zones.push_back({i, j});
             }
         }
     }
 
-    cout << max_area;
+    int max_safe_count = 0;
+    int safe_zone_count = (int)safe_zones.size();
+    int virus_zone_count = (int)virus_zones.size();
+    for (int i = 0; i < safe_zone_count - 2; i++) {
+        for (int j = i + 1; j < safe_zone_count - 1; j++) {
+            for (int k = j + 1; k < safe_zone_count; k++) {
+                memset(visited, 0, sizeof(visited));
+                memcpy(temp, lab, sizeof(lab));
+                temp[safe_zones[i].first][safe_zones[i].second] = 1;
+                temp[safe_zones[j].first][safe_zones[j].second] = 1;
+                temp[safe_zones[k].first][safe_zones[k].second] = 1;
+
+                int infected = 0;
+                queue<pair<int, int>> q;
+                for (auto& p : virus_zones) {
+                    q.push(p);
+                    visited[p.first][p.second] = true;
+                }
+                while (!q.empty()) {
+                    int current_y = q.front().first;
+                    int current_x = q.front().second;
+                    q.pop();
+                    
+                    for (int dir = 0; dir < dir_size; dir++) {
+                        int ny = dy[dir] + current_y;
+                        int nx = dx[dir] + current_x;
+                        if (0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx] && temp[ny][nx] == 0) {
+                            q.push({ny, nx});
+                            visited[ny][nx] = true;
+                            infected++;
+                        }
+                    }
+                }
+
+                int current_safe_count = safe_zone_count - infected - 3;
+                max_safe_count = max(max_safe_count, current_safe_count);
+            }
+        }
+    }
+
+    cout << max_safe_count << '\n';
 
     return 0;
 }
