e937f63c7f5c4a8834df1a2cf36f6b7ae3ed9770
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-07-19 19:05:11 +0900

    [Gold II] Title: 컵라면, Time: 48 ms, Memory: 5252 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,41 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <algorithm>
 
 using namespace std;
 
-int main(void)
-{
-    ios_base::sync_with_stdio(false);
+int main(void) {
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-    int N;
-    cin >> N;
-    vector<pair<int, int>> homeworks(N);
-    priority_queue<int, vector<int>, greater<int>> pq;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> homeworks[i].first >> homeworks[i].second;
+    vector<pair<int, int>> v(n);
+
+    for (auto& p : v) {
+        cin >> p.first >> p.second;
     }
 
-    sort(homeworks.begin(), homeworks.end());
+    sort(v.begin(), v.end());
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-    int total = 0;
-    for (int i = 0; i < N; i++)
-    {
-        pq.push(homeworks[i].second);
-        total += homeworks[i].second;
-        if (homeworks[i].first < pq.size())
-        {
-            total -= pq.top();
+    int result = 0;
+
+    for (auto& p : v) {
+        result += p.second;
+        pq.push(p.second);
+        if (p.first < (int)pq.size()) {
+            result -= pq.top();
             pq.pop();
         }
     }
 
-    cout << total << '\n';
+    cout << result << '\n';
 
     return 0;
 }
