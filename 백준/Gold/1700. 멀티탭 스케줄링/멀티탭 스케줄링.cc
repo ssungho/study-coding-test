e7a7ac95d455a3d25a6d0c8ee564ae1a1fec1b38
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-07-24 17:05:56 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,75 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <algorithm>
 
 using namespace std;
 
-int main(void)
-{
-    int N, K;
-    cin >> N >> K;
-    vector<int> v(K);
-    for (int i = 0; i < K; i++)
-    {
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(k);
+    for (int i = 0; i < k; i++) {
         cin >> v[i];
     }
 
-    int count = 0;
     set<int> s;
-    for (int i = 0; i < K; i++)
-    {
-        auto find_iter = s.find(v[i]);
-        if (s.size() == N)
-        {
-            if (find_iter == s.end())
-            {
-                int last_at = -1;
-                for (auto iter = s.begin(); iter != s.end();)
-                {
-                    int at = -1;
-                    for (int start = i + 1; start < K; start++)
-                    {
-                        if (*iter == v[start])
-                        {
-                            at = start;
-                            last_at = max(at, last_at);
-                            break;
-                        }
-                    }
+    int result = 0;
 
-                    if (at == -1)
-                    {
-                        count++;
-                        s.erase(iter);
-                        last_at = -1;
+    for (int i = 0; i < k; i++) {
+        auto find = s.find(v[i]);
+        if (s.size() < n) {
+            s.insert(v[i]);
+        } else if (find == s.end()) {
+            int erase = v[i];
+            int index = i;
+            for (auto temp : s) {
+                int pos = n;
+                for (int j = i; j < k; j++) {
+                    if (v[j] == temp) {
+                        pos = j;
                         break;
-                    }
-                    else
-                    {
-                        iter++;
-                    }
+                    } 
                 }
-
-                if (last_at != -1)
-                {
-                    count++;
-                    s.erase(v[last_at]);
+                
+                if (index < pos) {
+                    erase = v[pos];
+                    index = pos;
+                } else if (pos == n) {
+                    erase = temp;
+                    break;
                 }
+            }
 
-                s.insert(v[i]);
-            }
-        }
-        else
-        {
-            if (find_iter == s.end())
-            {
-                s.insert(v[i]);
-            }
+            s.erase(s.find(erase));
+            result++;
+            s.insert(v[i]);
         }
     }
-
-    cout << count << '\n';
+    
+    cout << result << '\n';
 
     return 0;
 }
