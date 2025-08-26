73db1a926271f8c52713a0a97051c336c5bc8a33
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-08-26 14:05:43 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 80 ms, Memory: 2188 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int Search(vector<int>& v, int target)
+{
+	int index = -1;
+	int left = 0;
+	int right = v.size() - 1;
+
+	while (left <= right)
+	{
+		int index_mid = (left + right) / 2;
+		int value_mid = v[index_mid];
+
+		if (value_mid < target)
+		{
+			index = index_mid;
+			left = index_mid + 1;
+		}
+		else
+		{
+			right = index_mid - 1;
+		}
+	}
+
+	return index;
+}
+
+int main(void)
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		vector<int> arr_a(a, 0), arr_b(b, 0);
+
+		for (int i = 0; i < a; i++)
+		{
+			cin >> arr_a[i];
+		}
+
+		for (int i = 0; i < b; i++)
+		{
+			cin >> arr_b[i];
+		}
+
+		sort(arr_b.begin(), arr_b.end());
+
+		int count = 0;
+        
+		for (int i = 0; i < a; i++)
+		{
+			int size_a = arr_a[i];
+			int index = Search(arr_b, size_a);
+            
+			if (-1 != index)
+			{
+				count += (index + 1);
+			}
+		}
+
+		cout << count << '\n';
+	}
+
+	return 0;
+}
