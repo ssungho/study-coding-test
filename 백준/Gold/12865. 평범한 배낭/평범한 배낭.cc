dc046fc49cf762b561b53c2205152402fb4a3601
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-09-01 17:37:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,41 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <memory.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int memo[101][100001]{};
+int n, k;
+int dp[100001]{};
+int items[101][2]{};
 
 int main(void)
 {
-    int N, K;
-    cin >> N >> K;
+	cin >> n >> k;
 
-    int wv[101][2]{};
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> items[i][0] >> items[i][1];
+	}
 
-    for (int i = 1; i <= N; i++)
-    {
-        cin >> wv[i][0] >> wv[i][1];
-    }
+	for (int i = 1; i <=n; i++) 
+	{
+		int w = items[i][0];
+		int v = items[i][1];
 
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= K; j++)
-        {
-            if (j < wv[i][0])
-            {
-                memo[i][j] = memo[i - 1][j];
-            }
-            else
-            {
-                int current_w = wv[i][0];
-                int current_v = wv[i][1];
-                memo[i][j] = max(memo[i - 1][j], memo[i - 1][j - current_w] + current_v);
-            }
-        }
-    }
+		for (int j = k; j >= w; j--)
+		{
+			dp[j] = max(dp[j], dp[j - w] + v);
+		}
+	}
+	
+	cout << dp[k] << '\n';
 
-    cout << memo[N][K];
-
-    return 0;
+	return 0;
 }
