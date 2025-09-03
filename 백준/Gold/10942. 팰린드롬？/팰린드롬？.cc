567bb3f9cba1b24aaee76d9fef0a212f2760c312
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-09-03 20:57:04 +0900

    [Gold IV] Title: 팰린드롬?, Time: 244 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,63 +1,59 @@
 #include <iostream>
+#include <memory.h>
+
 using namespace std;
 
-// https://www.acmicpc.net/problem/10942
-// boj 10942 팰린드롬?
+int n, m;
+int arr[2001]{};
+int dp[2001][2001]{};
 
-int arr[2001]; // 주어진 수열
-int memo[2001][2001]; // 2차원 dp table
-
-// 주어진 S, E가 팰린드롬인지 확인
-int Check(int S, int E)
+bool Check(int start, int end)
 {
-    // 값이 저장되어 있다면 그대로 반환
-    if(memo[S][E] != -1)
-        return memo[S][E];
+	if (dp[start][end] != -1)
+	{
+		return dp[start][end];
+	}
 
-    // 주어진 범위가 1칸 미만이거나 2칸이면 1 반환 (base)
-    if(E - S <= 0)
-        return memo[S][E] = 1;
+	if (start >= end)
+	{
+		return dp[start][end] = 1;
+	}
 
-    // arr[S]와 arr[E]가 다를 경우 0을 반환.
-    if (arr[S] != arr[E])
-        return memo[S][E] = 0;
+	if (arr[start] != arr[end])
+	{
+		return dp[start][end] = 0;
+	}
 
-    // S와 E사이가 팰리늗롬이라면 S와 E도 팰린드롬이다. 즉 1 반환
-    if(memo[S + 1][E - 1] == 1)
-        return memo[S][E] = 1;
-
-    // arr[S]와 arr[E]가 팰린드롬인지 확인하려면 사이의 범위가 팰린드롬인지 확인하면 된다
-    memo[S][E]  = Check(S + 1, E - 1);
-    return memo[S][E];
+	if (dp[start + 1][end - 1] == 1)
+	{
+		return dp[start][end] = 1;
+	}
+	
+	return dp[start][end] = Check(start + 1, end - 1);
 }
 
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int n;
-    cin >> n;
-    for (int i = 1; i < n + 1; i++)
-    {
-        int number;
-        cin >> number;
-        arr[i] = number;
-    }
+	memset(dp, -1, sizeof(dp));
 
-    for (int i = 1; i < n + 1; i++)
-        for (int j = 1; j < n + 1; j++)
-            memo[i][j] = -1;
-    
-    int m;
-    cin >> m;
-    for (int i = 0; i < m; i++)
-    {
-        int s, e;
-        cin >> s >> e;
-        cout << Check(s, e) << '\n';
-    }
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
 
-    return 0;
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		int s, e;
+		cin >> s >> e;
+		cout << Check(s, e) << '\n';
+	}
+
+	return 0;
 }
