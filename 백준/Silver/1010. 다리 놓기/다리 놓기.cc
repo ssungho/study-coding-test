f29d1f9802ad7844be3ab82338f21052ab47d65c
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-09-11 15:11:04 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,24 +1,33 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main(void)
 {
-    int T;
-    int N, M;
-    cin >> T;
-    int answer, temp;
-    while (T--)
-    {
-        answer = 1;
-        temp = 1;
-        cin >> N >> M;
-        for (int i = M; i > M - N; i--)
-        {
-            answer *= i;
-            answer /= temp++;
-        }
-        cout << answer << '\n';
-    }
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int dp[31][31]{};
+
+	for (int i = 0; i <= 30; i++)
+	{
+		dp[i][0] = 1;
+		dp[i][i] = 1;
+		for (int j = 1; j < i; j++)
+		{
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+		}
+	}
+
+	int t, n, m;
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> n >> m;
+		cout << dp[m][n] << '\n';
+	}
+
+	return 0;
 }
