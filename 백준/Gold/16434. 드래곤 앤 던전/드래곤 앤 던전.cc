e7679e24ca434fb0f1408ade31745d1e0e2d9c89
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-09-15 19:42:56 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 140 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,95 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
+using ull = unsigned long long;
 
-constexpr int monster = 1;
-constexpr int potion = 2;
-ll n, atk;
-vector<pair<ll, pair<ll, ll>>> v;
-
-bool Check(ll target)
+struct Info
 {
-	ll max_hp = target;
-	ll cur_hp = max_hp;
-	ll cur_atk = atk;
+    ull t, a, h;
+};
 
-	for (int i = 0; i < n; i++)
-	{
-		if (monster == v[i].first)
-		{
-			ll atk_monster = v[i].second.first;
-			ll hp_monster = v[i].second.second;
+ull n, init_atk, low, mid, high, result;
+vector<Info> v;
 
-			ll cnt_player_atk = hp_monster / cur_atk;
-			if (hp_monster % cur_atk)
-			{
-				cnt_player_atk++;
-			}
+bool Simulate(ull target)
+{
+    ull max_hp = target;
+    ull cur_hp = target;
+    ull cur_atk = init_atk;
 
-			ll cnt_monster_atk = cur_hp / atk_monster;
-			if (cur_hp % atk_monster)
-			{
-				cnt_monster_atk++;
-			}
+    for (const auto &info : v)
+    {
+        if (info.t == 1)
+        {
+            ull player_atk_cnt = info.h / cur_atk;
+            if (info.h % cur_atk) player_atk_cnt++;
 
-			if (cnt_player_atk > cnt_monster_atk)
-			{
-				return false;
-			}
+            ull monster_atk_cnt = cur_hp / info.a;
+            if (cur_hp % info.a) monster_atk_cnt++;
 
-			cur_hp -= (cnt_player_atk - 1) * atk_monster;
-		}
-		else
-		{
-			cur_atk += v[i].second.first;
-			cur_hp += v[i].second.second;
-			cur_hp = min(cur_hp, max_hp);
-		}
-	}
+            if (player_atk_cnt <= monster_atk_cnt)
+            {
+                cur_hp -= (player_atk_cnt - 1) * info.a;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            cur_atk += info.a;
+            cur_hp = min(cur_hp + info.h, max_hp);
+        }
 
-	return true;
+        if (cur_hp <= 0) return false;
+    }
+
+    return true;
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	cin >> n >> atk;
+    cin >> n >> init_atk;
 
-	v.resize(n);
+    v.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].t >> v[i].a >> v[i].h;
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i].first;
-		cin >> v[i].second.first;
-		cin >> v[i].second.second;
-	}
+    low = 1;
+    high = LONG_LONG_MAX;
+    result = LONG_LONG_MAX;
 
-	ll left = 0;
-	ll right = LONG_LONG_MAX;
-	ll result = 0;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
 
-	while (left <= right)
-	{
-		ll mid = (left + right) / 2;
+        if (true == Simulate(mid))
+        {
+            result = min(result, mid);
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
 
-		if (Check(mid))
-		{
-			result = mid;
-			right = mid - 1;
-		}
-		else
-		{
-			left = mid + 1;
-		}
-	}
+    cout << result << '\n';
 
-	cout << result << '\n';
-
-	return 0;
+    return 0;
 }
