44ac374805227c18c769ec5ae5a440f74620498c
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-09-19 15:51:33 +0900

    [Gold IV] Title: 최단경로, Time: 84 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,64 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
+
+vector<vector<pair<int, int>>> graph;
+vector<int> costs;
+
+int V, E, K;
+int u, v, w;
 
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int V, E;
-    cin >> V >> E;
+	cin >> V >> E >> K;
 
-    vector<vector<pair<int, int>>> graph(V + 1);
-    vector<int> memo(V + 1);
-    vector<bool> visited(V + 1);
+	graph.resize(V + 1);
+	costs.resize(V + 1, INT_MAX);
 
-    int start_node;
-    cin >> start_node;
+	for (int i = 0; i < E; i++)
+	{
+		cin >> u >> v >> w;
+		graph[u].push_back({w, v});
+	}
 
-    for (int i = 0; i < V + 1; i++)
-    {
-        memo[i] = INT32_MAX;
-        visited[i] = false;
-    }
-    for (int i = 0; i < E; i++)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        graph[u].push_back({v, w}); // 수정된 부분
-    }
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({0, K});
+	costs[K] = 0;
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, start_node}); // 가중치 노드
-    memo[start_node] = 0;
+	while (!pq.empty())
+	{
+		int cur_cost = pq.top().first;
+		int cur_node = pq.top().second;
+		pq.pop();
 
-    while (!pq.empty())
-    {
-        int curr_node = pq.top().second;
-    
-        pq.pop();
-        if (!visited[curr_node])
-        {
-            for (int i = 0; i < graph[curr_node].size(); i++)
-            {
-                memo[graph[curr_node][i].first] = min(memo[graph[curr_node][i].first], graph[curr_node][i].second + memo[curr_node]);
-                pq.push({memo[graph[curr_node][i].first], graph[curr_node][i].first});
-            }
-        }
-        visited[curr_node] = true;
-    }
+		if (costs[cur_node] < cur_cost)
+		{
+			continue;
+		}
 
-    for(int i = 1; i < V + 1; i++)
-    {
-        if(memo[i] == INT32_MAX)
-            cout << "INF" << '\n';
-        else
-            cout << memo[i] << '\n';
-    }
+		for (auto &next : graph[cur_node])
+		{
+			int next_cost = next.first;
+			int next_node = next.second;
 
-    return 0;
+			if (cur_cost + next_cost < costs[next_node])
+			{
+				costs[next_node] = cur_cost + next_cost;
+				pq.push({costs[next_node], next_node});
+			}
+		}
+	}
+
+	for (int i = 1; i <= V; i++)
+	{
+		if (costs[i] == INT_MAX)
+		{
+			cout << "INF\n";
+		}
+		else
+		{
+			cout << costs[i] << '\n';
+		}
+	}
+
+	return 0;
 }
