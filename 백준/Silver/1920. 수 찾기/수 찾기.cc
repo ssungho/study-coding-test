35e8c895fe8fe14bc1e2ac6c0c8c48ad6233e7a1
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-10-04 17:24:15 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,59 +2,26 @@
 
 using namespace std;
 
-int n, m, a[100000], target;
-
-int BinarySearch(int x)
-{
-	int result = 0;
-	int left = 0;
-	int right = n - 1;
-
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-
-		if (a[mid] == target)
-		{
-			result = 1;
-			break;
-		}
-
-		if (a[mid] < target)
-		{
-			left = mid + 1;
-		}
-		else
-		{
-			right = mid - 1;
-		}
-	}
-
-	return result;
-}
+int n, m, t, a[100001];
 
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	cout.tie(nullptr);
 
 	cin >> n;
-
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> a[i];
 	}
+	sort(a + 1, a + n + 1);
 
-	sort(a, a + n);
-	
 	cin >> m;
-
 	for (int i = 0; i < m; i++)
 	{
-		cin >> target;
-		cout << BinarySearch(target) << '\n';
+		cin >> t;
+		cout << binary_search(a + 1, a + n + 1, t) << '\n';
 	}
-
+	
 	return 0;
 }
