f4a8d8500fb51a91a4dc84871c1dd0143552cfa5
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-10-04 17:50:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 304 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,45 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int n, m, t, ret, a[500000];
+
 int main(void)
 {
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    
-	int n, m;
-
-	cin >> n;
-	
-	vector<int> v1;
-	for (int i = 0; i < n; i++)
-	{
-		int temp;
-		cin >> temp;
-		v1.push_back(temp);
-	}
-	
-	// 이진탐색관련 함수를 사용하기 위해 정렬..
-	std::sort(v1.begin(), v1.end());
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	cin >> m;
-
-	vector<int> v2;
 	for (int i = 0; i < m; i++)
 	{
-		int temp;
-		cin >> temp;
-		v2.push_back(temp);
+		cin >> a[i];
 	}
+	sort(a, a + m);
 
-	for (int i = 0; i < m; i++)
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		int cnt = 0;
-		cnt = upper_bound(v1.begin(), v1.end(), v2[i]) - lower_bound(v1.begin(), v1.end(), v2[i]);
-		cout << cnt << " ";
+		cin >> t;
+		ret = upper_bound(a, a + m, t) - lower_bound(a, a + m, t);
+		cout << ret << '\n';
 	}
 
 	return 0;
