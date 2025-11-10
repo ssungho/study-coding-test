bdfa0536a3ef57579814805c5b182dc73c6984c5
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-10 16:43:54 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,46 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int dx[2]{-1, 1};
-int n, m, j, a[21], result, pos_l = 1;
-bool visited[21];
-
-int bfs(int target_pos)
-{
-	int cnt = -1;
-	queue<pair<int, int>> q;
-	q.push({0, pos_l});
-	visited[pos_l] = true;
-
-	while (!q.empty())
-	{
-		int cur_cnt = q.front().first;
-		int cur_pos = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < m; i++)
-		{
-			if (target_pos == cur_pos + i)
-			{
-				pos_l = cur_pos;
-				cnt = cur_cnt;
-				return cnt;
-			}
-		}
-
-		for (int i = 0; i < 2; i++)
-		{
-			int new_pos = cur_pos + dx[i];
-			if (0 < new_pos && new_pos <= n && visited[new_pos] == false)
-			{
-				q.push({cur_cnt + 1, new_pos});
-				visited[new_pos] = true;
-			}
-		}
-	}
-
-	return cnt;
-}
+int n, m, j, a[21], result, pos_l, pos_r;
 
 int main(void)
 {
@@ -55,10 +16,26 @@ int main(void)
 		cin >> a[i];
 	}
 
+	pos_l = 1;
+	pos_r = pos_l + m - 1;
 	for (int i = 0; i < j; i++)
 	{
-		memset(visited, 0, sizeof(visited));
-		result += bfs(a[i]);
+		if (pos_l <= a[i] && a[i] <= pos_r)
+		{
+			continue;
+		}
+		else if (a[i] < pos_l)
+		{
+			result += (pos_l - a[i]);
+			pos_l = a[i];
+			pos_r = pos_l + m - 1;
+		}
+		else
+		{
+			result += (a[i] - pos_r);
+			pos_r = a[i];
+			pos_l = pos_r - m + 1;
+		}
 	}
 
 	cout << result << '\n';
