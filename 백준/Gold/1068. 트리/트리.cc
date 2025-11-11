ebab3cbb9cab775951fe124ffcd3d07da9eb64ff
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-11 15:26:30 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,74 +1,61 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr int ROOT{-1};
+int n, delete_node, root, parent;
+vector<vector<int>> children;
 
-void FindLeafNode(vector<vector<int>>& tree, int current, int& count)
+int go(int node)
 {
-    auto& child = tree[current];
+	if (node == delete_node)
+	{
+		return 0;
+	}
 
-    if (child.size() == 0) 
-    {
-        count++;
-    }
-    else 
-    {
-        for(auto i : child)
-        {
-            FindLeafNode(tree, i, count);
-        }
-    }
+	const auto &nodes = children[node];
+
+	if (nodes.size() == 0 || (nodes.size() == 1 && nodes[0] == delete_node))
+	{
+		return 1;
+	}
+
+	int cnt = 0;
+
+	for (int node : nodes)
+	{
+		if (node != delete_node)
+		{
+			cnt += go(node);
+		}
+	}
+
+	return cnt;
 }
 
-int main(void) 
+int main(void)
 {
-    int N;
-    cin >> N;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    vector<vector<int>> tree(N);
+	cin >> n;
+	children.resize(n);
 
-    int root_node;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> parent;
+		if (parent != -1)
+		{
+			children[parent].push_back(i);
+		}
+		else
+		{
+			root = i;
+		}
+	}
 
-    for (int i = 0; i < N; i++)
-    {
-        int parent;
-        cin >> parent;
+	cin >> delete_node;
 
-        if (parent == ROOT)
-        {
-            root_node = i;
-            continue;
-        }
+	cout << go(root) << '\n';
 
-        tree[parent].push_back(i);
-    }
-
-    int remove;
-    cin >> remove;
-
-    if (remove == root_node)
-    {
-        cout << 0;
-        return 0;
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        for (auto iter = tree[i].begin(); iter != tree[i].end();)
-        {
-            if (*iter == remove)
-                iter = tree[i].erase(iter);
-            else 
-                iter++;
-        }
-    }
-
-    int count = 0;
-    FindLeafNode(tree, root_node, count);
-
-    cout << count;
-
-    return 0;
+	return 0;
 }
