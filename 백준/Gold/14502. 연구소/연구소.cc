84656cc51716f7e25d1005ffe6aa6dcafd1acf2c
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-11 14:18:47 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,81 +1,94 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <memory.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr int dir_size{4};
-constexpr int arr_size{8};
-constexpr int dy[dir_size]{-1, 0, 1, 0};
-constexpr int dx[dir_size]{0, 1, 0, -1};
+const int dy[4]{-1, 0, 1, 0}, dx[4]{0, 1, 0, -1};
+int n, m, total, ny, nx, result, a[8][8];
+vector<pair<int, int>> virus_pos;
+vector<pair<int, int>> safe_pos;
+bool visited[8][8];
 
-int N, M;
-int lab[arr_size][arr_size] {};
-int temp[arr_size][arr_size] {};
-bool visited[arr_size][arr_size] {};
-vector<pair<int, int>> safe_zones;
-vector<pair<int, int>> virus_zones;
+int bfs()
+{
+	int ret = (int)safe_pos.size() + (int)virus_pos.size() - 3;
+	memset(visited, 0, sizeof(visited));
+	queue<pair<int, int>> q;
+	for (auto& pos : virus_pos)
+	{
+		q.push(pos);
+		visited[pos.first][pos.second] = true;
+	}
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+	while (!q.empty())
+	{
+		int y = q.front().first;
+		int x = q.front().second;
+		ret--;
+		q.pop();
 
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> lab[i][j];
-            if (lab[i][j] == 0) {
-                safe_zones.push_back({i, j});
-            }
-            else if (lab[i][j] == 2) {
-                virus_zones.push_back({i, j});
-            }
-        }
-    }
+		for (int i = 0; i < 4; i++)
+		{
+			ny = y + dy[i];
+			nx = x + dx[i];
 
-    int max_safe_count = 0;
-    int safe_zone_count = (int)safe_zones.size();
-    int virus_zone_count = (int)virus_zones.size();
-    for (int i = 0; i < safe_zone_count - 2; i++) {
-        for (int j = i + 1; j < safe_zone_count - 1; j++) {
-            for (int k = j + 1; k < safe_zone_count; k++) {
-                memset(visited, 0, sizeof(visited));
-                memcpy(temp, lab, sizeof(lab));
-                temp[safe_zones[i].first][safe_zones[i].second] = 1;
-                temp[safe_zones[j].first][safe_zones[j].second] = 1;
-                temp[safe_zones[k].first][safe_zones[k].second] = 1;
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m)
+				continue;
+			if (a[ny][nx] != 0 || visited[ny][nx] == true)
+				continue;
 
-                int infected = 0;
-                queue<pair<int, int>> q;
-                for (auto& p : virus_zones) {
-                    q.push(p);
-                    visited[p.first][p.second] = true;
-                }
-                while (!q.empty()) {
-                    int current_y = q.front().first;
-                    int current_x = q.front().second;
-                    q.pop();
-                    
-                    for (int dir = 0; dir < dir_size; dir++) {
-                        int ny = dy[dir] + current_y;
-                        int nx = dx[dir] + current_x;
-                        if (0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx] && temp[ny][nx] == 0) {
-                            q.push({ny, nx});
-                            visited[ny][nx] = true;
-                            infected++;
-                        }
-                    }
-                }
+			q.push({ny, nx});
+			visited[ny][nx] = true;
+		}
+	}
 
-                int current_safe_count = safe_zone_count - infected - 3;
-                max_safe_count = max(max_safe_count, current_safe_count);
-            }
-        }
-    }
+	return ret;
+}
 
-    cout << max_safe_count << '\n';
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    return 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> a[i][j];
+			if (a[i][j] == 0)
+			{
+				safe_pos.push_back({i, j});
+			}
+			else if (a[i][j] == 2)
+			{
+				virus_pos.push_back({i, j});
+			}
+		}
+	}
+
+	total = n * m;
+	int safe_cnt = (int)safe_pos.size();
+
+	for (int i = 0; i < safe_cnt - 2; i++)
+	{
+		for (int j = i + 1; j < safe_cnt - 1; j++)
+		{
+			for (int k = j + 1; k < safe_cnt; k++)
+			{
+				a[safe_pos[i].first][safe_pos[i].second] = 1;
+				a[safe_pos[j].first][safe_pos[j].second] = 1;
+				a[safe_pos[k].first][safe_pos[k].second] = 1;
+
+				result = max(result, bfs());
+
+				a[safe_pos[i].first][safe_pos[i].second] = 0;
+				a[safe_pos[j].first][safe_pos[j].second] = 0;
+				a[safe_pos[k].first][safe_pos[k].second] = 0;
+			}
+		}
+	}
+
+	cout << result << '\n';
+
+	return 0;
 }
