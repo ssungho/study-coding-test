7fa0c0eafe0d80ba0627520df1501b6dcbbcee0b
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-14 17:29:35 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int k, a_size, a[1025];
-vector<int> v[11];
+int k, a_size;
+int a[1025];
+vector<int> depth[11];
 
-void go(int s, int e, int depth)
+void go(int s, int e, int d)
 {
 	if (s > e)
 	{
 		return;
 	}
-
-	int mid = (s + e) / 2;
-	v[depth].push_back(a[mid]);
-	go(s, mid - 1, depth + 1);
-	go(mid + 1, e, depth + 1);
+	
+	int m = (s + e) / 2;
+	go(s, m - 1, d + 1);
+	depth[d].push_back(a[m]);
+	go(m + 1, e, d + 1);
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
 	cin >> k;
 
-	a_size = (int)pow(2, k);
-	for (int i = 1; i < a_size; i++)
+	a_size = (int)pow(2, k) - 1;
+	for (int i = 1; i <= a_size; i++)
 	{
 		cin >> a[i];
 	}
 
-	go(1, a_size - 1, 0);
+	go(1, a_size, 0);
 
 	for (int i = 0; i < k; i++)
 	{
-		for (auto i : v[i])
+		for (int node : depth[i])
 		{
-			cout << i << " ";
+			cout << node << ' ';
 		}
 		cout << '\n';
 	}
