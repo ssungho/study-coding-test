fdbed9ecf63d50c1f740380fd520541102349984
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-14 16:13:31 +0900

    [Gold IV] Title: 알파벳, Time: 392 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,62 +1,54 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr int DIR_SIZE = 4;
-int dy[DIR_SIZE]{-1, 1, 0, 0};
-int dx[DIR_SIZE]{0, 0, -1, 1};
-int R, C;
-int max_count = 0;
-bool visited[26] {};
+const int dy[4]{-1, 0, 1, 0}, dx[4]{0, 1, 0, -1};
+int r, c, result, a[21][21];
+bool visited[26];
 
-void DFS(vector<vector<char>> &map, int y, int x, int count)
+void go(int y, int x, int cnt)
 {
-    max_count = max(max_count, count);
+	visited[a[y][x]] = true;
+	result = max(result, cnt);
+	
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
 
-    for (int i = 0; i < DIR_SIZE; i++)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
+		if (ny < 0 || ny >= r || nx < 0 | nx >= c)
+		{
+			continue;
+		}
+		if (visited[a[ny][nx]])
+		{
+			continue;
+		}
 
-        if (0 < ny && ny <= R && 0 < nx && nx <= C)
-        {
-            int next = map[ny][nx] - 'A';
-
-            if (!visited[next])
-            {
-                visited[next] = true;
-                DFS(map, ny, nx, count + 1);
-                visited[next] = false;
-            }
-        }
-    }
+		go(ny, nx, cnt + 1);
+		visited[a[ny][nx]] = false;
+	}
 }
 
 int main(void)
 {
-    cin >> R >> C;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    vector<vector<char>> map(R + 1, vector<char>(C + 1));
+	cin >> r >> c;
+	for (int i = 0; i < r; i++)
+	{
+		string row;
+		cin >> row;
+		for (int j = 0; j < c; j++)
+		{
+			a[i][j] = int(row[j] - 'A');
+		}
+	}
 
-    for (int i = 1; i <= R; i++)
-    {
-        string row;
-        cin >> row;
+	go(0, 0, 1);
 
-        for (size_t j = 0; j < row.size(); j++)
-        {
-            map[i][j + 1] = row[j];
-        }
-    }
+	cout << result << '\n';
 
-    
-    visited[map[1][1] - 'A'] = true;
-
-    DFS(map, 1, 1, 1);
-
-    cout << max_count;
-
-    return 0;
+	return 0;
 }
