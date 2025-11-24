2f43e14fdacb42ae84defd655bdaf05d5bf1afd0
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-24 17:28:36 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1740 ms, Memory: 57124 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, k, num;
+char op;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> t;
+	while (t--)
+	{
+		priority_queue<int, vector<int>, greater<int>> pq_asc;
+		priority_queue<int> pq_desc;
+		map<int, int> visited;
+
+		cin >> k;
+		while (k--)
+		{
+			cin >> op >> num;
+			if (op == 'I')
+			{
+				pq_asc.push(num);
+				pq_desc.push(num);
+				visited[num]++;
+			}
+			else if (op == 'D')
+			{
+				if (num == -1 && pq_asc.size())
+				{
+					visited[pq_asc.top()]--;
+					pq_asc.pop();
+				}
+				if (num == 1 && pq_desc.size())
+				{
+					visited[pq_desc.top()]--;
+					pq_desc.pop();
+				}
+			}
+
+			while (pq_desc.size() && visited[pq_desc.top()] == 0)
+			{
+				pq_desc.pop();
+			}
+			while (pq_asc.size() && visited[pq_asc.top()] == 0)
+			{
+				pq_asc.pop();
+			}
+		}
+
+		if (pq_asc.empty() || pq_desc.empty())
+		{
+			cout << "EMPTY\n";
+		}
+		else
+		{
+			cout << pq_desc.top() << ' ' << pq_asc.top() << '\n';
+		}
+	}
+
+	return 0;
+}
