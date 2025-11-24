c2ed7113b6f205d2d4b5766f801a003bad84d5c6
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-11-24 17:41:54 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 612 ms, Memory: 32304 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using pii = pair<int, int>;
 
 int t, k, num;
 char op;
@@ -13,39 +14,40 @@ int main(void)
 	cin >> t;
 	while (t--)
 	{
-		priority_queue<int, vector<int>, greater<int>> pq_asc;
-		priority_queue<int> pq_desc;
-		unordered_map<int, int> visited;
-
 		cin >> k;
-		while (k--)
+
+		priority_queue<pii, vector<pii>, greater<pii>> pq_asc;
+		priority_queue<pii> pq_desc;
+		vector<bool> visited(k);
+
+		for (int i = 0; i < k; i++) 
 		{
 			cin >> op >> num;
 			if (op == 'I')
 			{
-				pq_asc.push(num);
-				pq_desc.push(num);
-				visited[num]++;
+				pq_asc.push({num, i});
+				pq_desc.push({num, i});
+				visited[i] = true;
 			}
 			else if (op == 'D')
 			{
 				if (num == -1 && pq_asc.size())
 				{
-					visited[pq_asc.top()]--;
+					visited[pq_asc.top().second] = false;
 					pq_asc.pop();
 				}
 				if (num == 1 && pq_desc.size())
 				{
-					visited[pq_desc.top()]--;
+					visited[pq_desc.top().second] = false;
 					pq_desc.pop();
 				}
 			}
 
-			while (pq_desc.size() && visited[pq_desc.top()] == 0)
+			while (pq_desc.size() && visited[pq_desc.top().second] == false)
 			{
 				pq_desc.pop();
 			}
-			while (pq_asc.size() && visited[pq_asc.top()] == 0)
+			while (pq_asc.size() && visited[pq_asc.top().second] == false)
 			{
 				pq_asc.pop();
 			}
@@ -57,7 +59,7 @@ int main(void)
 		}
 		else
 		{
-			cout << pq_desc.top() << ' ' << pq_asc.top() << '\n';
+			cout << pq_desc.top().first << ' ' << pq_asc.top().first << '\n';
 		}
 	}
 
