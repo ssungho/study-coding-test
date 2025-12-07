077b350f75e0dee522356b54e70bbf270adbd554
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2025-12-07 14:26:58 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.cc
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -2,33 +2,26 @@
 using namespace std;
 
 string s, word;
-int cnt, max_cnt;
+int cnt, pos;
 
 int main(void)
 {
 	getline(cin, s);
 	getline(cin, word);
-	
-	for (int i = 0; i < word.size(); i++)
+
+	while (true)
 	{
-		cnt = 0;
-		for (int j = i; j < (int)s.size(); j++)
+		pos = s.find(word, pos);
+		if (pos == string::npos)
 		{
-			int pos = s.find(word, j);
-			if (pos != string::npos)
-			{
-				cnt++;
-				j = pos + word.size() - 1;
-			}
-			else
-			{
-				break;
-			}
+			break;
 		}
-		max_cnt = max(max_cnt, cnt);
+
+		cnt++;
+		pos += (int)word.size();
 	}
 
-	cout << max_cnt << '\n';
-	
+	cout << cnt << '\n';
+
 	return 0;
 }
