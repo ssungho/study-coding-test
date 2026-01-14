5f12eaa7854d6c085dd404f8958f7bd6c1dbc826
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2026-01-14 21:31:08 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 276 ms, Memory: 4804 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, a, b;
+int indegrees[1001], dp[1001];
+
+int main(void)
+{
+	cin >> n >> m;
+
+	vector<vector<int>> graph(n + 1);
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b;
+		graph[a].push_back(b);
+		indegrees[b]++;
+	}
+
+	queue<pair<int, int>> q;
+	for (int i = 1; i <= n; i++)
+	{
+		if (indegrees[i] == 0)
+		{
+			q.push({i, 0});
+		}
+	}
+
+	while (!q.empty())
+	{
+		int sub = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+
+		dp[sub] = cnt + 1;
+
+		for (int next : graph[sub])
+		{
+			if (--indegrees[next] == 0)
+			{
+				q.push({next, cnt + 1});
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << dp[i] << ' ';
+	}
+	cout << '\n';
+
+	return 0;
+}
