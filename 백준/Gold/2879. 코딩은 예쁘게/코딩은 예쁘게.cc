030eb1c24dca4d83ecd58d81f08ea5c9a6e94144
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2026-02-23 19:30:14 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, result, arr[1001];
-pair<int, int> gap[1001];
+int n, result, src[1001], dst[1001];
 
 int main(void)
 {
@@ -12,64 +11,33 @@ int main(void)
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> arr[i];
+		cin >> src[i];
 	}
-
+	
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> gap[i].second;
-		gap[i].second -= arr[i];
-
-		if (gap[i].second > 0)
-		{
-			gap[i].first = 1;
-		}
-		else if (gap[i].second < 0)
-		{
-			gap[i].first = -1;
-		}
+		cin >> dst[i];
 	}
 
-	int start = 1;
-	
-	while (true)
+	int prev = 0;
+	for (int i = 1; i <= n; i++) 
 	{
-		while (start <= n && gap[start].first == 0)
+		int diff = dst[i] - src[i];
+
+		bool check1 = (diff > 0 && prev < 0);
+		bool check2 = (diff < 0 && prev > 0);
+
+		if (check1 || check2)
 		{
-			start++;
+        	prev = 0;
 		}
 
-		if (start > n)
+		if (abs(diff) > abs(prev))
 		{
-			break;
+			result += abs(diff) - abs(prev);
 		}
 
-		int flag = gap[start].first;
-		int cnt = abs(gap[start].second);
-		int here = start;
-
-		while (true)
-		{
-			if (flag == gap[here].first && here <= n)
-			{
-				cnt = min(cnt, abs(gap[here].second));
-				here++;
-			}
-			else
-			{
-				for (int i = start; i < here; i++)
-				{
-					flag == 1 ? gap[i].second -= cnt : gap[i].second += cnt;
-					if (gap[i].second == 0)
-					{
-						gap[i].first = 0;
-					}
-				}
-
-				result += cnt;
-				break;
-			}
-		}
+		prev = diff;
 	}
 
 	cout << result << '\n';
