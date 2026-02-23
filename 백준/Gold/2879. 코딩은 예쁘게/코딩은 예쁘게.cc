395cb4cf60ea55eed293660589c22f87fc9b4954
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2026-02-23 19:17:56 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, result, arr[1001];
+pair<int, int> gap[1001];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> gap[i].second;
+		gap[i].second -= arr[i];
+
+		if (gap[i].second > 0)
+		{
+			gap[i].first = 1;
+		}
+		else if (gap[i].second < 0)
+		{
+			gap[i].first = -1;
+		}
+	}
+
+	int start = 1;
+	bool finish = false;
+
+	while (true)
+	{
+		while (start <= n && gap[start].first == 0)
+		{
+			start++;
+		}
+
+		if (start > n)
+		{
+			break;
+		}
+
+		int flag = gap[start].first;
+		int cnt = abs(gap[start].second);
+		int here = start;
+
+		while (true)
+		{
+			if (flag == gap[here].first && here <= n)
+			{
+				cnt = min(cnt, abs(gap[here].second));
+				here++;
+			}
+			else
+			{
+				for (int i = start; i < here; i++)
+				{
+					flag == 1 ? gap[i].second -= cnt : gap[i].second += cnt;
+					if (gap[i].second == 0)
+					{
+						gap[i].first = 0;
+					}
+				}
+
+				result += cnt;
+				break;
+			}
+		}
+	}
+
+	cout << result << '\n';
+
+	return 0;
+}
