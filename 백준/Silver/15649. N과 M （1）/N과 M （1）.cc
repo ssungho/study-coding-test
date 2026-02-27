285957babaec30d67d265db1eb0a71e3eb56a5b5
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2026-02-27 16:49:49 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,41 +1,38 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<int> v;
-bool visited[9];
 int n, m;
+bool visited[9];
+vector<int> v;
 
-void go(int cur, int cnt)
+void go(int here, int cnt)
 {
-    if (cnt == m)
-    {
-        for (int i : v)
-        {
-            cout << i << " ";
-        }
-        cout << '\n';
-        return;
-    }
+	if (cnt == m)
+	{
+		for (auto i : v)
+		{
+			cout << i << ' ';
+		}
+		cout << '\n';
+		return;
+	}
 
-    for (int i = 1; i <= n; i++)
-    {
-        if (!visited[i])
-        {
-            visited[i] = true;
-            v.push_back(i);
-            go(i + 1, cnt + 1);
-            v.pop_back();
-            visited[i] = false;
-        }
-    }
+	for (int i = 1; i <= n; i++)
+	{
+		if (visited[i] == false)
+		{
+			visited[i] = true;
+			v.push_back(i);
+			go(i + 1, cnt + 1);
+			v.pop_back();
+			visited[i] = false;
+		}
+	}
 }
 
 int main(void)
 {
-    cin >> n >> m;
-
-    go(1, 0);
-
-    return 0;
+	cin >> n >> m;
+	go(1, 0);
+	return 0;
 }
