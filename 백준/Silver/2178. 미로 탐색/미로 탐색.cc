5da425c36e02f5583d31e686a6d056ea24276fa8
SungHo Ahn <103364628+ssungho@users.noreply.github.com>
2026-03-17 11:06:16 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-const int dy[4]{-1, 0, 1, 0}, dx[4]{0, 1, 0, -1};
-int n, m, ret;
-char a[101][101];
+const int dy[]{-1, 0, 1, 0}, dx[]{0, 1, 0, -1};
+int n, m, result, a[101][101];
 bool visited[101][101];
 string row;
 
 int main(void)
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++)
 	{
@@ -33,20 +34,20 @@ int main(void)
 
 		if (cur_y == n && cur_x == m)
 		{
-			ret = cur_cnt;
+			result = cur_cnt;
 			break;
 		}
 
 		for (int dir = 0; dir < 4; dir++)
 		{
-			int ny = cur_y + dy[dir];
-			int nx = cur_x + dx[dir];
+			int ny = dy[dir] + cur_y;
+			int nx = dx[dir] + cur_x;
 
 			if (ny < 1 || ny > n || nx < 1 || nx > m)
 			{
 				continue;
 			}
-			if (a[ny][nx] == '0' || visited[ny][nx] == true)
+			if (a[ny][nx] == '0' || visited[ny][nx])
 			{
 				continue;
 			}
@@ -56,7 +57,7 @@ int main(void)
 		}
 	}
 
-	cout << ret << '\n';
+	cout << result << '\n';
 
 	return 0;
 }
